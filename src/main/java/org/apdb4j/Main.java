05697e2b40769a5a875dac4e619cda7812b5f507
Sofia Cipollone <sofia.cipollone5@gmail.com>
2023-09-03 19:42:02 +0200

    Added sample test query

--- src/main/java/org/apdb4j/Main.java
+++ src/main/java/org/apdb4j/Main.java
@@ -1,7 +1,33 @@
 package org.apdb4j;
 
+import io.github.cdimascio.dotenv.Dotenv;
+import org.jooq.DSLContext;
+import org.jooq.Record;
+import org.jooq.Result;
+import org.jooq.impl.DSL;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import static org.apdb4j.db.Tables.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Dotenv dotenv = Dotenv.load();
+        var url = dotenv.get("DB_URL");
+        var user = dotenv.get("DB_USERNAME");
+        var password = dotenv.get("DB_PASSWORD");
+        // DB connection sample.
+        try (Connection connection = DriverManager.getConnection(url, user, password)) {
+            DSLContext db = DSL.using(connection);
+            Result<Record> result = db.select().from(PRICE_LISTS).fetch();
+            for (Record r : result) {
+                var year = r.getValue(PRICE_LISTS.YEAR);
+                System.out.println("YEAR: " + year);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
