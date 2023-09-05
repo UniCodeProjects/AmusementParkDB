75e6eebd53442a1842c4656d07ecd8aa922bb1c0
Sofia Cipollone <sofia.cipollone5@gmail.com>
2023-09-05 21:51:30 +0200

    Added QueryBuilder with examples in Main

--- src/main/java/org/apdb4j/Main.java
+++ src/main/java/org/apdb4j/Main.java
@@ -1,33 +1,41 @@
 package org.apdb4j;
 
-import io.github.cdimascio.dotenv.Dotenv;
-import org.jooq.DSLContext;
-import org.jooq.Record;
-import org.jooq.Result;
-import org.jooq.impl.DSL;
-
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import org.apdb4j.util.QueryBuilder;
+//import org.jooq.Record;
+//import org.jooq.Result;
 
 import static org.apdb4j.db.Tables.*;
 
-public class Main {
-    public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-        var url = dotenv.get("DB_URL");
-        var user = dotenv.get("DB_USERNAME");
-        var password = dotenv.get("DB_PASSWORD");
+/**
+ * Main class.
+ */
+public final class Main {
+
+    private Main() {
+    }
+
+    /**
+     * Main method.
+     * @param args main args.
+     */
+    public static void main(final String[] args) {
         // DB connection sample.
-        try (Connection connection = DriverManager.getConnection(url, user, password)) {
-            DSLContext db = DSL.using(connection);
-            Result<Record> result = db.select().from(PRICE_LISTS).fetch();
-            for (Record r : result) {
-                var year = r.getValue(PRICE_LISTS.YEAR);
-                System.out.println("YEAR: " + year);
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        final var builder = new QueryBuilder();
+        builder.createConnection()
+                .queryAction(db -> db.insertInto(PRICE_LISTS).values(2015).execute())
+                .closeConnection();
+
+//        builder.createConnection()
+//                .queryAction(db -> {
+//                    final Result<Record> result = db
+//                            .select()
+//                            .from(PRICE_LISTS)
+//                            .fetch();
+//                    for (final Record r : result) {
+//                        final var year = r.getValue(PRICE_LISTS.YEAR);
+//                        System.out.println("YEAR: " + year);
+//                    }
+//                })
+//                .closeConnection();
     }
 }
