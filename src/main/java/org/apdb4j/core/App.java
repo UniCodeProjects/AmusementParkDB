e6c95305ad64d7065898c7d0fb66660df17d836c
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2023-09-08 20:57:23 +0200

    Added JavaFX application entry point

--- src/main/java/org/apdb4j/core/App.java
+++ src/main/java/org/apdb4j/core/App.java
@@ -1,37 +1,40 @@
 package org.apdb4j.core;
 
 import javafx.application.Application;
-import javafx.geometry.Orientation;
-import javafx.geometry.Pos;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.FlowPane;
 import javafx.stage.Stage;
 
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.io.IOException;
+
 /**
- *
+ * GUI entry point.
  */
 public class App extends Application {
+
+    private static final double WIDTH_FACTOR = 0.8;
+    private static final double HEIGHT_FACTOR = 0.8;
+    private static final double MIN_WIDTH_FACTOR = 0.5;
+    private static final double MIN_HEIGHT_FACTOR = 0.5;
 
     /**
      * {@inheritDoc}
      */
     @Override
-    public void start(final Stage primaryStage) {
-        final Button btn = new Button();
-        final TextField text = new TextField();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(event -> text.setText("Hello World!"));
+    public void start(final Stage primaryStage) throws IOException {
+        final Parent root = FXMLLoader.load(ClassLoader.getSystemResource("layouts/signin-screen.fxml"));
+        final Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        final var width = screenSize.getWidth() * WIDTH_FACTOR;
+        final var height = screenSize.getHeight() * HEIGHT_FACTOR;
+        final Scene scene = new Scene(root, width, height);
+        root.requestFocus();
 
-        final FlowPane root = new FlowPane();
-        root.setOrientation(Orientation.VERTICAL);
-        root.setAlignment(Pos.CENTER);
-        root.getChildren().addAll(btn, text);
-
-        final Scene scene = new Scene(root, 300, 250);
-
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setMinWidth(width * MIN_WIDTH_FACTOR);
+        primaryStage.setMinHeight(height * MIN_HEIGHT_FACTOR);
+        primaryStage.setTitle("APDB4J");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
