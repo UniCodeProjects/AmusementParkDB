f30cab4e1909099f01fe82435e220b91acdc470c
Sofia Cipollone <sofia.cipollone5@gmail.com>
2023-09-08 11:39:25 +0200

    Added sample JavaFX application, removed old main

--- src/main/java/org/apdb4j/core/Main.java
+++ src/main/java/org/apdb4j/core/Main.java
@@ -1,12 +1,6 @@
 package org.apdb4j.core;
 
-import org.apdb4j.core.permissions.Permission;
-import org.apdb4j.core.permissions.PermissionDeniedException;
-import org.apdb4j.util.QueryBuilder;
-//import org.jooq.Record;
-//import org.jooq.Result;
-
-import static org.apdb4j.db.Tables.*;
+import javafx.application.Application;
 
 /**
  * Main class.
@@ -20,28 +14,7 @@ public final class Main {
      * Main method.
      * @param args main args.
      */
-    public static void main(final String[] args) throws PermissionDeniedException {
-        // DB connection sample.
-        final var p = new Permission() {
-        };
-        final var builder = new QueryBuilder();
-        builder.definePermissions(p, p)
-                .createConnection()
-                .queryAction(db -> db.insertInto(PRICE_LISTS).values(2015).execute())
-                .closeConnection();
-
-//        builder.definePermissions(p, p)
-//                .createConnection()
-//                .queryAction(db -> {
-//                    final Result<Record> result = db
-//                            .select()
-//                            .from(PRICE_LISTS)
-//                            .fetch();
-//                    for (final Record r : result) {
-//                        final var year = r.getValue(PRICE_LISTS.YEAR);
-//                        System.out.println("YEAR: " + year);
-//                    }
-//                })
-//                .closeConnection();
+    public static void main(final String[] args) {
+        Application.launch(App.class, args);
     }
 }
