a34c78fac9ec35ce45223110a78aacf2a6c9da90
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2023-09-09 00:21:59 +0200

    Added JavaFXUtils, added username in stage title after login, cleanup in LoadFXML

--- src/main/java/org/apdb4j/view/SignInController.java
+++ src/main/java/org/apdb4j/view/SignInController.java
@@ -7,6 +7,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import org.apdb4j.util.JavaFXUtils;
 import org.apdb4j.util.LoadFXML;
 
 import java.awt.Toolkit;
@@ -38,6 +39,7 @@ public class SignInController implements Initializable {
     void signIn(final ActionEvent event) {
         // todo: check user type, add validator.
         if (!username.getText().isBlank() || !password.getText().isBlank()) {
+            JavaFXUtils.setStageTitle(event, username.getText());
             LoadFXML.fromEvent(event, "layouts/staff-screen.fxml", false);
         }
     }
