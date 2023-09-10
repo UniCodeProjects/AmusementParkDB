46655c1fe80010caa78b305a9f0bd51211d0aa2c
Sofia Cipollone <sofia.cipollone5@gmail.com>
2023-09-10 19:28:15 +0200

    Added prompts and chart in expenses page

--- src/main/java/org/apdb4j/view/StaffScreenController.java
+++ src/main/java/org/apdb4j/view/StaffScreenController.java
@@ -1,29 +1,51 @@
 package org.apdb4j.view;
 
+import javafx.collections.ListChangeListener;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
 /**
  * The FXML controller for the staff UI.
  */
-public class StaffScreenController {
+public class StaffScreenController implements Initializable {
 
+    @FXML
+    private Button addRowBtn;
+    @FXML
+    private Button deleteAllRowsBtn;
+    @FXML
+    private LineChart<?, ?> chart;
     @FXML
     private Button clearBtn;
     @FXML
     private MenuBar menuBar;
     @FXML
     private ToggleGroup radioBtnToggle;
+    @FXML
+    private VBox vBox;
+    private int addRowCounter = 1;
+    private static final int MAX_ROWS = 10;
 
     /**
      * Opens a popup window to change the user's password.
@@ -58,6 +80,124 @@ public class StaffScreenController {
     @FXML
     void onClearBtn(final ActionEvent event) {
         radioBtnToggle.getSelectedToggle().setSelected(false);
+    }
+
+    /**
+     * Adds a new row of date pickers and relative buttons.
+     * @param event the event
+     */
+    @FXML
+    void addRow(final ActionEvent event) {
+        final var label = new Label("Net profit");
+        final var datePicker1 = new DatePicker();
+        final var datePicker2 = new DatePicker();
+        datePicker1.setPromptText("Start date");
+        datePicker1.setMaxWidth(Double.MAX_VALUE);
+        datePicker2.setPromptText("End date");
+        datePicker2.setMaxWidth(Double.MAX_VALUE);
+
+        final var clearBtn = new Button("Clear");
+        clearBtn.setMaxWidth(Double.MAX_VALUE);
+        clearBtn.setDisable(true);
+        final var deleteBtn = new Button("Delete");
+        deleteBtn.setMaxWidth(Double.MAX_VALUE);
+
+        final var spacing = 20;
+        final var container = new HBox(spacing, label, datePicker1, datePicker2, clearBtn, deleteBtn);
+        container.setAlignment(Pos.CENTER);
+        container.setPadding(new Insets(5, 5, 5, 5));
+
+        addListenersToDatePicker(datePicker1, datePicker2, clearBtn);
+
+        // Adds the clear feature for the button.
+        clearBtn.setOnAction(e -> clearDatePickers(datePicker1, datePicker2, clearBtn));
+
+        // Adds the delete feature for the button.
+        deleteBtn.setOnAction(e -> {
+            vBox.getChildren().remove(container);
+            addRowCounter--;
+            if (addRowCounter < MAX_ROWS) {
+                addRowBtn.setDisable(false);
+            }
+        });
+
+        vBox.getChildren().add(container);
+        addRowCounter++;
+        if (addRowCounter >= MAX_ROWS) {
+            addRowBtn.setDisable(true);
+        }
+    }
+
+    /**
+     * Clears the date pickers' text input.
+     * @param event the event
+     */
+    @FXML
+    void clearRowBtn(final ActionEvent event) {
+        final HBox firstRow = (HBox) vBox.getChildren().get(0);
+        final DatePicker datePicker1 = (DatePicker) firstRow.getChildren().get(1);
+        final DatePicker datePicker2 = (DatePicker) firstRow.getChildren().get(2);
+        final Button clearButton = (Button) firstRow.getChildren().get(3);
+        clearDatePickers(datePicker1, datePicker2, clearButton);
+    }
+
+    /**
+     * Deletes all the added rows.
+     * @param event the event
+     */
+    @FXML
+    void deleteAllRows(final ActionEvent event) {
+        vBox.getChildren().remove(1, vBox.getChildrenUnmodifiable().size());
+        addRowCounter = 1;
+        addRowBtn.setDisable(false);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void initialize(final URL location, final ResourceBundle resources) {
+        /*
+         * Disables or enables the delete all rows button
+         * based on the presence of at least one new row.
+         */
+        vBox.getChildren().addListener((ListChangeListener<Node>) change -> {
+            while (change.next()) {
+                deleteAllRowsBtn.setDisable(!change.wasAdded());
+            }
+        });
+        // Sets the default first row.
+        final HBox firstRow = (HBox) vBox.getChildren().get(0);
+        final DatePicker datePicker1 = (DatePicker) firstRow.getChildren().get(1);
+        final DatePicker datePicker2 = (DatePicker) firstRow.getChildren().get(2);
+        final Button clearButton = (Button) firstRow.getChildren().get(3);
+        clearButton.setDisable(true);
+        addListenersToDatePicker(datePicker1, datePicker2, clearButton);
+    }
+
+    private static void addListenersToDatePicker(final DatePicker datePicker1,
+                                                 final DatePicker datePicker2,
+                                                 final Button clearButton) {
+        datePicker1.valueProperty().addListener(observable -> clearButton.setDisable(false));
+        datePicker2.valueProperty().addListener(observable -> clearButton.setDisable(false));
+        datePicker1.getEditor().textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.isEmpty()) {
+                clearButton.setDisable(false);
+            }
+        });
+        datePicker2.getEditor().textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.isEmpty()) {
+                clearButton.setDisable(false);
+            }
+        });
+    }
+
+    private static void clearDatePickers(final DatePicker datePicker1,
+                                         final DatePicker datePicker2,
+                                         final Button clearButton) {
+        datePicker1.getEditor().setText("");
+        datePicker2.getEditor().setText("");
+        clearButton.setDisable(true);
     }
 
 }
