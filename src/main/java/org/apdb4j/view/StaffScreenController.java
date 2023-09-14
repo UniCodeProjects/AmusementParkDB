6d2734131f724dec5fe209c59a569bc0a7791070
Sofia Cipollone <sofia.cipollone5@gmail.com>
2023-09-14 16:09:45 +0200

    Fixed NullPointerException in StaffScreenController

--- src/main/java/org/apdb4j/view/StaffScreenController.java
+++ src/main/java/org/apdb4j/view/StaffScreenController.java
@@ -11,11 +11,7 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.*;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Modality;
@@ -23,6 +19,7 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 import java.util.ResourceBundle;
 
 /**
@@ -79,7 +76,10 @@ public class StaffScreenController implements Initializable {
      */
     @FXML
     void onClearBtn(final ActionEvent event) {
-        radioBtnToggle.getSelectedToggle().setSelected(false);
+        final Toggle selectedToggle = radioBtnToggle.getSelectedToggle();
+        if (Objects.nonNull(selectedToggle)) {
+            selectedToggle.setSelected(false);
+        }
     }
 
     /**
