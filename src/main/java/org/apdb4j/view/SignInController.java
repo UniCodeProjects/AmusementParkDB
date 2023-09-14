bd8519e1c69b5df5471d5de069d0fd4091b57ecf
Sofia Cipollone <sofia.cipollone5@gmail.com>
2023-09-14 13:44:30 +0200

    Added event handlers to enable/disable the log-in buttons

--- src/main/java/org/apdb4j/view/SignInController.java
+++ src/main/java/org/apdb4j/view/SignInController.java
@@ -14,7 +14,9 @@ import org.apdb4j.util.view.LoadFXML;
 
 import java.awt.Toolkit;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Stream;
 
 /**
  * The FXML controller for the sign-in scene.
@@ -40,7 +42,7 @@ public class SignInController implements Initializable {
     @FXML
     void signIn(final ActionEvent event) {
         // todo: check user type, add validator.
-        if (!username.getText().isBlank() || !password.getText().isBlank()) {
+        if (!username.getText().isBlank() && !password.getText().isBlank()) {
             JavaFXUtils.setStageTitle(event, username.getText());
             LoadFXML.fromEvent(event, "layouts/staff-screen.fxml", false);
         }
@@ -76,6 +78,20 @@ public class SignInController implements Initializable {
         username.setPrefWidth(width);
         password.setPrefWidth(width);
         signInBtn.setPrefWidth(width);
+        signInBtn.setDisable(true);
+        List.of(username, password).forEach(textField -> {
+            textField.setOnKeyTyped(event -> {
+                if (Stream.of(username, password).map(TextField::getText).noneMatch(String::isBlank)) {
+                    signInBtn.setDisable(false);
+                }
+            });
+            textField.setOnKeyPressed(event -> {
+                if (event.getCode().equals(KeyCode.BACK_SPACE) || event.getCode().equals(KeyCode.DELETE)
+                        && Stream.of(username, password).map(TextField::getText).anyMatch(String::isBlank)) {
+                    signInBtn.setDisable(true);
+                }
+            });
+        });
     }
 
 }
