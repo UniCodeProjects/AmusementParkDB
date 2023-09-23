274dbef3e9136f088370f9d0ffcc0c7ab74d6864
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2023-09-23 21:51:33 +0200

    wip: code refactoring in PermissionUID

--- src/main/java/org/apdb4j/core/permissions/PermissionUID.java
+++ src/main/java/org/apdb4j/core/permissions/PermissionUID.java
@@ -4,41 +4,56 @@ import lombok.NonNull;
 import lombok.SneakyThrows;
 import org.apache.commons.collections4.MultiValuedMap;
 import org.apache.commons.collections4.multimap.ArrayListValuedHashMap;
-import org.apdb4j.core.permissions.account.GuestPermission;
+import org.apdb4j.util.QueryBuilder;
+import org.apdb4j.util.RegexUtils;
+import org.reflections.Reflections;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.Objects;
+
+import static org.apdb4j.db.tables.Permissions.PERMISSIONS;
 
 /**
+ * todo: update doc
  * Maps an Access class instance to an actual database tuple.
  * @see Access
  */
-// TODO: make this class the generator, instead of the validator. Make methods not static?
-public final class PermissionMapper {
+public final class PermissionUID {
 
-    private static Access source;
+    private final Access source;
+    private final String uid;
     private static final String CLASS_NAME_PATTERN_TO_MATCH = "Permission";
 
-    private PermissionMapper() {
+    /**
+     * Creates a new UID from a permission class.
+     * @param source the permission object
+     */
+    public PermissionUID(final @NonNull Access source) {
+        this.source = source;
+        uid = assemblePermissionUID();
     }
 
     /**
-     * Inserts a tuple in the database by using
-     * the provided access object as a data source.
-     * @param source the access object
+     * Inserts a tuple in the database representing a permission.
+     * @return {@code true} on successful insertion
      */
-    public static void insertInDB(final @NonNull Access source) {
-        PermissionMapper.source = source;
-//        result.append(getSimplePermissionName(source));
-        System.out.println(assemblePermissionUID());
+    public boolean insertInDB() {
+        final var type = getSimplePermissionName(source);
+        final int insertedTuples = new QueryBuilder()
+                .createConnection()
+                .queryAction(db -> db.insertInto(PERMISSIONS)
+                        .values(type, uid)
+                        .execute())
+                .closeConnection()
+                .getResultAsInt();
+        return insertedTuples == 1;
     }
 
-    private static @NonNull String assemblePermissionUID() {
+    private @NonNull String assemblePermissionUID() {
         final MultiValuedMap<String, String> permissionMap = generateAllInterfaceCodes();
         final List<String> sortedKeys = permissionMap.keySet().stream().sorted().toList();
         final var result = new StringBuilder();
@@ -48,17 +63,17 @@ public final class PermissionMapper {
             final String interfaceCode = values.get(1);
             final String accessSequence = values.get(2);
             result.append(packageCode)
-                    .append(".")
+                    .append('.')
                     .append(interfaceCode)
-                    .append(".")
+                    .append('.')
                     .append(accessSequence)
-                    .append("-");
+                    .append('-');
         });
         result.deleteCharAt(result.length() - 1);
         return result.toString();
     }
 
-    private static @NonNull String getSimplePermissionName(final @NonNull Access permission) {
+    private @NonNull String getSimplePermissionName(final @NonNull Access permission) {
         final String className = permission.getClass().getSimpleName();
         if (!className.contains(CLASS_NAME_PATTERN_TO_MATCH)) {
             throw new IllegalStateException("Wrong class name format: "
@@ -67,17 +82,16 @@ public final class PermissionMapper {
         return className.replace(CLASS_NAME_PATTERN_TO_MATCH, "");
     }
 
-    private static @NonNull String generatePackageCodes(final @NonNull String packageName) {
+    private @NonNull String generatePackageCodes(final @NonNull String packageName) {
         final var regex = "(?<=\\.)\\w+$";
-        return "P" + (getMatch(packageName, regex))
+        return "P" + (RegexUtils.getMatch(packageName, regex).orElseThrow())
                 .toUpperCase(Locale.ROOT)
                 .charAt(0);
     }
 
     @SneakyThrows
-    private static @NonNull MultiValuedMap<String, String> generateAllInterfaceCodes() {
-        final var allInterfaces = PermissionConsistencyValidator.getInstance().getKnownAccessInterfacesNames();
-        // TODO: remember to sort! Not a treemap.
+    private @NonNull MultiValuedMap<String, String> generateAllInterfaceCodes() {
+        final var allInterfaces = getKnownAccessInterfacesNames();
         final MultiValuedMap<String, String> interfaceCodesMap = new ArrayListValuedHashMap<>();
         for (final String i : allInterfaces) {
             final var actualInterface = Class.forName(i).asSubclass(Access.class);
@@ -96,16 +110,16 @@ public final class PermissionMapper {
         return interfaceCodesMap;
     }
 
-    // TODO: cleanup.
-    // BUGFIX: incorrect N amount.
+    // todo: cleanup.
     @SneakyThrows
-    private static @NonNull String generateReturnSequence(final @NonNull String interfaceName) {
+    private @NonNull String generateReturnSequence(final @NonNull String interfaceName) {
         final Class<? extends Access> actualInterface = Class.forName(interfaceName).asSubclass(Access.class);
         // Check if interfaceName is implemented by source.
         if (Arrays.asList(source.getClass().getInterfaces()).contains(actualInterface)) {
             // Gets only the methods that are actively implemented by source.
             final var methods = Arrays.stream(source.getClass().getDeclaredMethods())
-                    .filter(method -> Arrays.stream(actualInterface.getMethods()).anyMatch(m -> m.getName().equals(method.getName())))
+                    .filter(method -> Arrays.stream(actualInterface.getMethods())
+                            .anyMatch(m -> m.getName().equals(method.getName())))
                     .sorted(Comparator.comparing(Method::getName))
                     .toList();
             final var sequence = new StringBuilder();
@@ -114,11 +128,11 @@ public final class PermissionMapper {
             }
             return sequence.toString();
         }
-        return "N".repeat(actualInterface.getDeclaredMethods().length);
+        return AccessType.NONE.toString().repeat(actualInterface.getDeclaredMethods().length);
     }
 
     @SneakyThrows
-    private static @NonNull String generateInterfaceCode(final @NonNull String interfaceName) {
+    private @NonNull String generateInterfaceCode(final @NonNull String interfaceName) {
         return Class.forName(interfaceName)
                 .getSimpleName()
                 .substring(0, 3)
@@ -126,7 +140,7 @@ public final class PermissionMapper {
     }
 
     @SneakyThrows
-    private static @NonNull String generateInterfaceCode(final @NonNull String interfaceName, final int numericalCode) {
+    private @NonNull String generateInterfaceCode(final @NonNull String interfaceName, final int numericalCode) {
         final int numericalCodeLength = String.valueOf(numericalCode).length();
         final String numCodeAsString = switch (numericalCodeLength) {
             case 1 -> "0" + numericalCode;
@@ -139,19 +153,41 @@ public final class PermissionMapper {
                 .toUpperCase(Locale.ROOT) + numCodeAsString;
     }
 
-    private static @NonNull String getMatch(final @NonNull String string, final @NonNull String regex) {
-        final Pattern pattern = Pattern.compile(regex);
-        final Matcher matcher = pattern.matcher(string);
-        String match = "";
-        if (matcher.find()) {
-            match = matcher.group();
-        }
-        return match;
+    private List<String> getKnownAccessInterfacesNames() {
+        final Reflections reflections = new Reflections("org.apdb4j.core.permissions");
+        return reflections.getSubTypesOf(Access.class).stream()
+                .filter(Class::isInterface)
+                .map(Class::getName)
+                .sorted()
+                .toList();
     }
 
-    // TODO: remove.
-    public static void main(String[] args) {
-        PermissionMapper.insertInDB(new GuestPermission());
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        final PermissionUID that = (PermissionUID) o;
+        return Objects.equals(uid, that.uid);
     }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(uid);
+    }
+
+    // todo: remove.
+//    public static void main(String[] args) {
+//        System.out.println(new PermissionUID(new GuestPermission()).insertInDB());
+//    }
 
 }
