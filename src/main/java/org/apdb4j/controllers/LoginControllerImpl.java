117454142e9ec519dceb0c3b111a98492518c9ce
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2023-09-28 00:22:39 +0200

    Added getErrorMessage in Controller interface, finished implementation of LoginController

--- src/main/java/org/apdb4j/controllers/LoginControllerImpl.java
+++ src/main/java/org/apdb4j/controllers/LoginControllerImpl.java
@@ -4,6 +4,9 @@ import lombok.NonNull;
 import org.apdb4j.util.QueryBuilder;
 import org.jooq.Record;
 import org.jooq.Result;
+import org.jooq.exception.DataAccessException;
+
+import java.util.Optional;
 
 import static org.apdb4j.db.Tables.ACCOUNTS;
 
@@ -15,6 +18,7 @@ import static org.apdb4j.db.Tables.ACCOUNTS;
 public class LoginControllerImpl implements LoginController {
 
     private static final QueryBuilder DB = new QueryBuilder();
+    private String errorMessage;
 
     /**
      * {@inheritDoc}
@@ -30,7 +34,11 @@ public class LoginControllerImpl implements LoginController {
                 .closeConnection()
                 .getResultAsRecords();
         // Checking if got only one result, and it is unique (accounts are unique).
-        return resultCount.size() == 1 && resultCount.get(0).get(0, Integer.class) == 1;
+        if (resultCount.size() == 1 && resultCount.get(0).get(0, Integer.class) == 1) {
+            return true;
+        }
+        errorMessage = "No account found, please check and try again.";
+        return false;
     }
 
     /**
@@ -38,7 +46,27 @@ public class LoginControllerImpl implements LoginController {
      */
     @Override
     public boolean checkSignUp(final @NonNull String email, final @NonNull String username, final @NonNull String password) {
-        return false;
+        int queryResult;
+        try {
+            queryResult = DB.createConnection()
+                    .queryAction(db -> db.insertInto(ACCOUNTS, ACCOUNTS.EMAIL, ACCOUNTS.USERNAME, ACCOUNTS.PASSWORD)
+                            .values(email, username, password)
+                            .execute())
+                    .closeConnection()
+                    .getResultAsInt();
+        } catch (final DataAccessException e) {
+            errorMessage = e.getCause().toString();
+            return false;
+        }
+        return queryResult == 1;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public @NonNull Optional<String> getErrorMessage() {
+        return Optional.ofNullable(errorMessage);
     }
 
 }
