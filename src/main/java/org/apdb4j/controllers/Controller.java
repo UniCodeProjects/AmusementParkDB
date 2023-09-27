deee87c82c6ca7e6fea0ef76a787c9451811e78f
Sofia Cipollone <sofia.cipollone5@gmail.com>
2023-09-28 00:22:39 +0200

    Added getErrorMessage in Controller interface, finished implementation of LoginController

--- src/main/java/org/apdb4j/controllers/Controller.java
+++ src/main/java/org/apdb4j/controllers/Controller.java
@@ -1,7 +1,20 @@
 package org.apdb4j.controllers;
 
+import lombok.NonNull;
+
+import java.util.Optional;
+
 /**
  * A tag interface that groups all MVC controllers.
  */
 public interface Controller {
+
+    /**
+     * Retrieves the error message caused by a failed operation.
+     * @return {@link Optional}{@code <String>} containing the error message,<br>
+     *         {@code empty} if there is no error message to retrieve,
+     *         or the controller does not provide any error message forwarding.
+     */
+    @NonNull Optional<String> getErrorMessage();
+
 }
