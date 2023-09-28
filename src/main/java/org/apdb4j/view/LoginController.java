7091d24ce0684e842dad5a30eecb82e7b4b7faa9
Sofia Cipollone <sofia.cipollone5@gmail.com>
2023-09-28 23:13:38 +0200

    Added LoginController to group common methods of sign-in and sign-up controllers

---
+++ src/main/java/org/apdb4j/view/LoginController.java
@@ -0,0 +1,34 @@
+package org.apdb4j.view;
+
+import javafx.application.Platform;
+import javafx.scene.control.Alert;
+import lombok.AccessLevel;
+import lombok.Getter;
+import org.apdb4j.controllers.LoginControllerImpl;
+
+/**
+ * The login fxml controller. Groups all common functionalities
+ * between the sign-in and sign-up controllers.
+ * @see SignInController
+ * @see SignUpController
+ */
+public class LoginController {
+
+    @Getter(AccessLevel.PACKAGE)
+    private final org.apdb4j.controllers.LoginController controller = new LoginControllerImpl();
+
+    /**
+     * Shows a simple error dialog window.<br>
+     * The shown error message is given by the MVC controller.
+     * If no message was given, a simple "Error" text will be displayed.
+     */
+    void showErrorDialog() {
+        Platform.runLater(() -> {
+            final var alert = new Alert(Alert.AlertType.ERROR);
+            alert.setHeaderText("An error has occurred.");
+            alert.setContentText(controller.getErrorMessage().orElse("Error"));
+            alert.show();
+        });
+    }
+
+}
