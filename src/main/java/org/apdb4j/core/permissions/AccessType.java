dd0c79e1beed4eeae252922ffd5d205b5f7d2390
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2023-10-03 21:58:25 +0200

    Improved AccessType

--- src/main/java/org/apdb4j/core/permissions/AccessType.java
+++ src/main/java/org/apdb4j/core/permissions/AccessType.java
@@ -4,53 +4,76 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 /**
- * The enum that represents the access type of the database.<br>
- * It provides access to an attribute on two levels:
+ * Represents the access type of the database.<br>
+ * It provides access to an attribute on three levels:
  * <ul>
  *     <li>{@code GLOBAL}: allows the user to access the attribute all across every tuple that contains it.</li>
  *     <li>{@code LOCAL}: allows the user to access only its own attribute.</li>
+ *     <li>{@code NONE}: forbids the user to access the attribute on all access levels.</li>
  * </ul>
  */
-@Getter
-@AllArgsConstructor
-public enum AccessType {
-    /**
-     * The user can read this database's specific attribute on a global level.
-     */
-    GLOBAL_READ('R'),
-    /**
-     * The user can read this database's specific attribute on a local level.
-     */
-    LOCAL_READ('r'),
-    /**
-     * The user can write on this database's specific attribute on a global level.
-     */
-    GLOBAL_WRITE('W'),
-    /**
-     * The user can write on this database's specific attribute on a local level.
-     */
-    LOCAL_WRITE('w'),
-    /**
-     * The user can read and write on this database's specific attribute on a global level.
-     */
-    GLOBAL_ALL('A'),
-    /**
-     * The user can read and write on this database's specific attribute on a local level.
-     */
-    LOCAL_ALL('a'),
-    /**
-     * The user has no access for this database's specific attribute on a global level.
-     */
-    NONE('N');
-
-    private final char type;
+public interface AccessType {
 
     /**
-     * {@inheritDoc}
+     * Represents the {@code Read AccessType}.
      */
-    @Override
-    public String toString() {
-        return String.valueOf(type);
+    @Getter
+    @AllArgsConstructor
+    enum Read {
+        /**
+         * The user can read this database's specific attribute on a global level.
+         */
+        GLOBAL('R'),
+        /**
+         * The user can read this database's specific attribute on a local level.
+         */
+        LOCAL('r'),
+        /**
+         * The user has no access for this database's specific attribute on all levels.
+         */
+        NONE('N');
+
+        private final char type;
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public String toString() {
+            return String.valueOf(type);
+        }
+
+    }
+
+    /**
+     * Represents the {@code Write AccessType}.
+     */
+    @Getter
+    @AllArgsConstructor
+    enum Write {
+        /**
+         * The user can write on this database's specific attribute on a global level.
+         */
+        GLOBAL('W'),
+        /**
+         * The user can write on this database's specific attribute on a local level.
+         */
+        LOCAL('w'),
+        /**
+         * The user has no access for this database's specific attribute on all levels.
+         */
+        NONE('N');
+
+        private final char type;
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public String toString() {
+            return String.valueOf(type);
+        }
+
     }
 
 }
