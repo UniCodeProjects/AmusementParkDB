c25d91f7c20a2a30726c7ad7b91e444f39a53694
Sofia Cipollone <sofia.cipollone5@gmail.com>
2023-10-07 13:41:33 +0200

    Edited UID generation according to changes, wip: uid parsing

---
+++ src/main/java/org/apdb4j/core/permissions/uid/UIDParser.java
@@ -0,0 +1,20 @@
+package org.apdb4j.core.permissions.uid;
+
+import lombok.NonNull;
+
+/**
+ * A parser for the {@code PermissionUID}.
+ */
+public final class UIDParser {
+
+    private UIDParser() {
+    }
+
+    /**
+     * Retrieves the actual contents of the UID.
+     * @param uid the uid
+     */
+    public static void parse(final @NonNull String uid) {
+
+    }
+}
