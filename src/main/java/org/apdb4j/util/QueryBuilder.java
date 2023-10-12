d8ebe36b002488dad746fd0cf8cd803faf547dd6
Sofia Cipollone <sofia.cipollone5@gmail.com>
2023-10-12 21:35:34 +0200

    wip: fixed bugs in 'definePermissions' of QueryBuilder, added and fixed tests

--- src/main/java/org/apdb4j/util/QueryBuilder.java
+++ src/main/java/org/apdb4j/util/QueryBuilder.java
@@ -2,31 +2,29 @@ package org.apdb4j.util;
 
 import io.github.cdimascio.dotenv.Dotenv;
 import lombok.NonNull;
-import org.apache.commons.lang3.tuple.Pair;
 import org.apdb4j.core.permissions.Access;
 import org.apdb4j.core.permissions.AccessDeniedException;
 import org.apdb4j.core.permissions.AccessSetting;
 import org.apdb4j.core.permissions.AccessType;
 import org.apdb4j.core.permissions.uid.AppPermissionUID;
 import org.apdb4j.core.permissions.uid.DBPermissionUID;
-import org.apdb4j.core.permissions.uid.PackageInterfaceSequence;
 import org.apdb4j.core.permissions.uid.ReturnSequence;
-import org.apdb4j.core.permissions.uid.Sequence;
 import org.apdb4j.core.permissions.uid.UID;
+import org.apdb4j.core.permissions.uid.UIDParser;
+import org.apdb4j.core.permissions.uid.UIDSection;
 import org.jooq.DSLContext;
 import org.jooq.Record;
 import org.jooq.Result;
-import org.jooq.Table;
-import org.jooq.TableField;
 import org.jooq.impl.DSL;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Objects;
-import java.util.Optional;
 import java.util.Set;
 import java.util.function.Function;
 
@@ -39,31 +37,26 @@ public class QueryBuilder {
     private Object queryResult;
 
     /**
-     * Defines the access that allows the execution of the following query(ies).
-     * @param requiredAccess the required permission
-     * @param requiredTable the required table
-     * @param recordTableField the required table field
-     * @param values the required access setting values
-     * @param actualAccountEmail the account's email used to check its permissions from the database
+     * Defines the requiredPermission that allows the execution of the following query(ies).
+     * @param checkingValues the values that will check the validity of the actual values
+     * @param actualValues the values to check
      * @return {@link QueryBuilder} for fluent style
-     * @throws AccessDeniedException when the access objects are incompatible
+     * @throws AccessDeniedException when the requiredPermission objects are incompatible
      */
-    public QueryBuilder definePermissions(final @NonNull Access requiredAccess,
-                                          final @NonNull TableField<Record, ?> recordTableField,
-                                          final @NonNull AccessSetting values,
-                                          final @NonNull String actualAccountEmail) throws AccessDeniedException {
-        if (invalidAccess(requiredAccess, recordTableField, values, actualAccountEmail)) {
+    public QueryBuilder definePermissions(final @NonNull CheckingValues checkingValues,
+                                          final @NonNull ActualValues actualValues) throws AccessDeniedException {
+        if (invalidAccess(checkingValues, actualValues)) {
             throw new AccessDeniedException();
         }
         return this;
     }
 
     /**
-     * Defines the access that allows the execution of the following query(ies).
+     * Defines the requiredPermission that allows the execution of the following query(ies).
      * @param required the required permissions
      * @param actualAccountEmail the account's email used to check its permissions from the database
      * @return {@link QueryBuilder} for fluent style
-     * @throws AccessDeniedException when the access objects are incompatible
+     * @throws AccessDeniedException when the requiredPermission objects are incompatible
      */
     public QueryBuilder definePermissions(final @NonNull Set<Access> required,
                                           final @NonNull String actualAccountEmail) throws AccessDeniedException {
@@ -169,56 +162,78 @@ public class QueryBuilder {
         }
     }
 
-    private boolean invalidAccess(final @NonNull Access requiredAccess,
-                                  final @NonNull TableField<Record, ?> recordTableField,
-                                  final @NonNull AccessSetting values,
-                                  final @NonNull String actualAccountEmail) {
+    private boolean invalidAccess(final @NonNull CheckingValues checkingValues,
+                                  final @NonNull ActualValues actualValues) {
         final UID uidFromDb;
         try {
-            uidFromDb = new DBPermissionUID(actualAccountEmail).getUid();
+            uidFromDb = new DBPermissionUID(checkingValues.email()).getUid();
         } catch (final NoSuchElementException e) {
-            System.out.println("Catching...");
+            // The provided email does not exist in the DB.
             return true;
         }
-        if (Objects.equals(new AppPermissionUID(requiredAccess).getUid(), uidFromDb)) {
-            System.out.println("UID !=");
+        // The UID from the database and the one just generated are not equals.
+        if (!Objects.equals(new AppPermissionUID(checkingValues.requiredPermission()).getUid(), uidFromDb)) {
+            return true;
+        }
+        final List<UIDSection> parsed = UIDParser.parse(uidFromDb.uid());
+        // If all attributes are empty and READ and WRITE are set to GLOBAL, it is an admin permission.
+        final boolean isAdmin = parsed.stream()
+                .map(UIDSection::returnSequence)
+                .allMatch(returnSequences -> returnSequences.stream()
+                        .allMatch(returnSequence -> returnSequence.getAttribute().isEmpty()
+                                && returnSequence.getRead().equals(AccessType.Read.GLOBAL)
+                                && returnSequence.getWrite().equals(AccessType.Write.GLOBAL)));
+        if (isAdmin) {
             return false;
         }
-        // TODO: null pointer from getInterface.
-        final var interfaceFromHash = ((PackageInterfaceSequence) PackageInterfaceSequence.getFromHash(uidFromDb.uid())).getInterface();
-        if (interfaceFromHash.equals(requiredAccess.getClass())) {
-            return false;
-        }
-        final ReturnSequence returnSequenceFromHash = (ReturnSequence) ReturnSequence.getFromHash(uidFromDb.uid());
-        final Optional<TableField<Record, ?>> attributeFromHash = returnSequenceFromHash.getAttribute();
-        if (attributeFromHash.isPresent() && attributeFromHash.get().equals(recordTableField)
-                && values.getReadAccess().equals(getReadAccessFromDb(uidFromDb, returnSequenceFromHash))
-                && values.getWriteAccess().equals(getWriteAccessFromDb(uidFromDb, returnSequenceFromHash))) {
-            return false;
-        }
-        if (attributeFromHash.isEmpty()
-                && values.getReadAccess().equals(getReadAccessFromDb(uidFromDb, returnSequenceFromHash))
-                && values.getWriteAccess().equals(getWriteAccessFromDb(uidFromDb, returnSequenceFromHash))) {
-            return false;
-        }
-        return true;
-    }
-
-    private Pair<AccessType.Read, Set<Class<? extends Access>>> getReadAccessFromDb(final UID uid, final Sequence returnSequence) {
-        final AccessType.Read readFromHash = ((ReturnSequence) returnSequence).getRead();
-        final Set<Class<? extends Access>> readTargetsFromHash = ((ReturnSequence) ReturnSequence.getFromHash(uid.uid())).getReadTargets();
-        return Pair.of(readFromHash, readTargetsFromHash);
-    }
-
-    private Pair<AccessType.Write, Set<Class<? extends Access>>> getWriteAccessFromDb(final UID uid, final Sequence returnSequence) {
-        final AccessType.Write writeFromHash = ((ReturnSequence) returnSequence).getWrite();
-        final Set<Class<? extends Access>> writeTargetsFromHash = ((ReturnSequence) ReturnSequence.getFromHash(uid.uid())).getWriteTargets();
-        return Pair.of(writeFromHash, writeTargetsFromHash);
+        // Otherwise, it is a normal permission.
+        final var returnSequences = parsed.stream()
+                .map(UIDSection::returnSequence)
+                .flatMap(Collection::stream)
+                .toList();
+        return actualValues.getValues().stream()
+                .allMatch(accessSetting -> returnSequences.stream()
+                        .noneMatch(sequence -> sequence.equals(new ReturnSequence(accessSetting))));
     }
 
     private boolean invalidAccess(final @NonNull Collection<Access> required, final @NonNull String actual) {
         return required.stream()
                 .noneMatch(req -> Objects.equals(new AppPermissionUID(req).getUid(), new DBPermissionUID(actual).getUid()));
+    }
+
+    /**
+     * Contains the values that will check the validity of the {@link ActualValues}.
+     * @param requiredPermission the required permission to perform the query
+     * @param email the account's email used to check its permissions from the database
+     * @see ActualValues
+     */
+    public record CheckingValues(@NonNull Access requiredPermission, @NonNull String email) {
+    }
+
+    /**
+     * Contains the values to check.
+     * @see CheckingValues
+     */
+    public static class ActualValues {
+
+        private final List<AccessSetting> values;
+
+        /**
+         * Creates a new {@code ActualValues} instance.
+         * @param values the values to check
+         */
+        public ActualValues(final @NonNull AccessSetting... values) {
+            this.values = List.of(values);
+        }
+
+        /**
+         * Returns the values to check.
+         * @return a list containing a copy of the values
+         */
+        public List<AccessSetting> getValues() {
+            return new ArrayList<>(values);
+        }
+
     }
 
 }
--- src/test/java/org/apdb4j/util/QueryBuilderTest.java
+++ src/test/java/org/apdb4j/util/QueryBuilderTest.java
@@ -10,8 +10,7 @@ import org.apdb4j.core.permissions.AccessType;
 import org.apdb4j.core.permissions.AdminPermission;
 import org.apdb4j.core.permissions.GuestPermission;
 import org.apdb4j.core.permissions.StaffPermission;
-import org.jooq.Record;
-import org.jooq.TableField;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -20,7 +19,9 @@ import java.util.Set;
 import java.util.stream.Stream;
 
 import static org.apdb4j.db.Tables.ACCOUNTS;
+import static org.apdb4j.db.Tables.FACILITIES;
 import static org.apdb4j.db.Tables.PICTURES;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SuppressWarnings("PMD.AvoidDuplicateLiterals")
@@ -31,29 +32,43 @@ class QueryBuilderTest {
     @SuppressFBWarnings("UPM_UNCALLED_PRIVATE_METHOD")
     private static Stream<Arguments> accessDeniedTestCases() {
         return Stream.of(
-                Arguments.of(new AdminPermission(),
-                        ACCOUNTS.USERNAME,
-                        AccessSetting.of(ACCOUNTS.USERNAME, AccessType.Read.NONE, AccessType.Write.NONE),
-                        "mariorossi@gmail.com"),
+                // Wrong permission case
                 Arguments.of(new StaffPermission(),
-                        PICTURES.PATH,
-                        AccessSetting.of(PICTURES.PATH, AccessType.Read.LOCAL, AccessType.Write.LOCAL),
-                        "sofiaverdi@gmail.com"),
+                        "mariorossi@gmail.com",
+                        AccessSetting.of(ACCOUNTS.USERNAME, AccessType.Read.GLOBAL, AccessType.Write.GLOBAL)),
+                // Wrong email case
+                Arguments.of(new AdminPermission(),
+                        "sofiaverdi@gmail.com",
+                        AccessSetting.of(PICTURES.PATH, AccessType.Read.GLOBAL, AccessType.Write.GLOBAL),
+                // Wrong AccessSetting case
                 Arguments.of(new GuestPermission(),
-                        ACCOUNTS.EMAIL,
-                        AccessSetting.of(ACCOUNTS.EMAIL, AccessType.Read.GLOBAL, Pair.of(AccessType.Write.GLOBAL, Set.of(GuestPermission.class))),
-                        "foo.bar@mail.com")
-        );
+                        "alessandrogialli@gmail.com",
+                        AccessSetting.of(ACCOUNTS.EMAIL,
+                                AccessType.Read.GLOBAL,
+                                Pair.of(AccessType.Write.GLOBAL, Set.of(GuestPermission.class))))
+        ));
     }
 
     @ParameterizedTest
     @MethodSource("accessDeniedTestCases")
-    void accessDeniedTest(final @NonNull Access requiredAccess,
-                          final @NonNull TableField<Record, ?> recordTableField,
-                          final @NonNull AccessSetting values,
-                          final @NonNull String actualAccountEmail) {
+    void accessDeniedTest(final @NonNull Access requiredPermission,
+                          final @NonNull String actualAccountEmail,
+                          final @NonNull AccessSetting values) {
         assertThrows(AccessDeniedException.class,
-                () -> DB.definePermissions(requiredAccess, recordTableField, values, actualAccountEmail));
+                () -> DB.definePermissions(new QueryBuilder.CheckingValues(requiredPermission, actualAccountEmail),
+                        new QueryBuilder.ActualValues(values)));
+    }
+
+    @Test
+    void adminPermissionTest() {
+        assertDoesNotThrow(() -> DB.definePermissions(
+                new QueryBuilder.CheckingValues(new AdminPermission(), "mariorossi@gmail.com"),
+                new QueryBuilder.ActualValues(AccessSetting.of(ACCOUNTS.USERNAME, AccessType.Read.NONE, AccessType.Write.NONE))));
+        assertDoesNotThrow(() -> DB.definePermissions(
+                new QueryBuilder.CheckingValues(new GuestPermission(), "alessandrogialli@gmail.com"),
+                new QueryBuilder.ActualValues(AccessSetting.of(Set.of(FACILITIES.OPENINGTIME, FACILITIES.CLOSINGTIME),
+                        AccessType.Read.GLOBAL,
+                        AccessType.Write.NONE))));
     }
 
 }
