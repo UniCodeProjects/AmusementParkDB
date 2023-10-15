3589d2e41a9a547a9d730d4b07be5afe5689dc50
Raffaele Francesco Marrazzo <raffaele.marrazzo@studio.unibo.it>
2023-10-15 20:11:55 +0200

    Implemented MaintenanceManager methods

--- src/main/java/org/apdb4j/core/managers/MaintenanceManager.java
+++ src/main/java/org/apdb4j/core/managers/MaintenanceManager.java
@@ -4,7 +4,11 @@ import java.time.LocalDate;
 import java.util.List;
 
 import lombok.NonNull;
+import org.apdb4j.util.QueryBuilder;
 import org.jooq.Record;
+
+import static org.apdb4j.db.Tables.MAINTENANCES;
+import static org.apdb4j.db.Tables.RESPONSIBILITIES;
 
 /**
  * Contains all the SQL queries that are related to the {@link org.apdb4j.db.tables.Maintenances} table.
@@ -27,14 +31,31 @@ public final class MaintenanceManager {
      * @param employeeNIDs the employees that are responsible for the maintenance. If also one of the values
      *                     of this parameter is not the national identifier of an employee,
      *                     the query will not be executed.
+     * @return {@code true} if the maintenances is inserted, {@code false} otherwise.
      */
-    public static void addNewMaintenance(final @NonNull String facilityID,
+    public static boolean addNewMaintenance(final @NonNull String facilityID,
                                   final double price,
                                   final @NonNull String description,
                                   final @NonNull LocalDate date,
                                   final @NonNull String account,
                                   final @NonNull String... employeeNIDs) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        final var maintenancesInserted = new QueryBuilder().createConnection()
+                .queryAction(db -> db.insertInto(MAINTENANCES)
+                        .values(facilityID, price, description, date)
+                        .execute())
+                .closeConnection()
+                .getResultAsInt();
+        final var builder = new QueryBuilder();
+        var responsibilitiesInserted = 0;
+        for (final var employee : employeeNIDs) {
+            responsibilitiesInserted += builder.createConnection()
+                    .queryAction(db -> db.insertInto(RESPONSIBILITIES)
+                            .values(facilityID, date, employee)
+                            .execute())
+                    .closeConnection()
+                    .getResultAsInt();
+        }
+        return maintenancesInserted == 1 && responsibilitiesInserted == employeeNIDs.length;
     }
 
     /**
@@ -45,7 +66,13 @@ public final class MaintenanceManager {
      * @return a list containing all the park's facilities, sorted by the date of their last maintenance.
      */
     public static @NonNull List<Record> sortFacilitiesByLastMaintenanceDate(final @NonNull String account) {
-        throw new UnsupportedOperationException("Not implemented yet");
+        return new QueryBuilder().createConnection()
+                .queryAction(db -> db.select()
+                        .from(MAINTENANCES)
+                        .orderBy(MAINTENANCES.DATE.desc())
+                        .fetch())
+                .closeConnection()
+                .getResultAsRecords();
     }
 
     // viewAllPlannedMaintenances();
