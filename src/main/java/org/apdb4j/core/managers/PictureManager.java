a53d0fe53d0c0bb3a9bcd5c0058449b5abe9b9fd
Raffaele Francesco Marrazzo <raffaele.marrazzo@studio.unibo.it>
2023-10-15 22:35:52 +0200

    Implemented PictureManager methods

--- src/main/java/org/apdb4j/core/managers/PictureManager.java
+++ src/main/java/org/apdb4j/core/managers/PictureManager.java
@@ -1,6 +1,9 @@
 package org.apdb4j.core.managers;
 
 import lombok.NonNull;
+import org.apdb4j.util.QueryBuilder;
+
+import static org.apdb4j.db.Tables.PICTURES;
 
 /**
  * Contains all the SQL queries that are related to the {@link org.apdb4j.db.tables.Pictures} table.
@@ -17,10 +20,16 @@ public final class PictureManager {
      *                      the identifier of a park service, the query will not be executed.
      * @param account the account that is performing this operation. If this account has not the permissions
      *                to accomplish the operation, the query will not be executed.
+     * @return {@code true} if the picture is inserted successfully, {@code false} otherwise.
      */
-    public static void addPicture(final @NonNull String picturePath, final @NonNull String parkServiceID,
-                                  final @NonNull String account) {
-        throw new UnsupportedOperationException("Not implemented yet");
+    public static boolean addPicture(final @NonNull String picturePath, final @NonNull String parkServiceID,
+                                     final @NonNull String account) {
+        return new QueryBuilder().createConnection()
+                .queryAction(db -> db.insertInto(PICTURES)
+                        .values(picturePath, parkServiceID)
+                        .execute())
+                .closeConnection()
+                .getResultAsInt() == 1;
     }
 
     /**
@@ -30,9 +39,15 @@ public final class PictureManager {
      * @param newPath the new path of the picture.
      * @param account the account that is performing this operation. If this account has not the permissions
      *                to accomplish the operation, the query will not be executed.
+     * @return {@code true} if the picture is edited successfully, {@code false} otherwise.
      */
-    public static void editPicture(final @NonNull String oldPath, final @NonNull String newPath, final @NonNull String account) {
-        throw new UnsupportedOperationException("Not implemented yet");
+    public static boolean editPicture(final @NonNull String oldPath, final @NonNull String newPath,
+                                      final @NonNull String account) {
+        return new QueryBuilder().createConnection()
+                .queryAction(db -> db.update(PICTURES).set(PICTURES.PATH, newPath)
+                        .where(PICTURES.PATH.eq(oldPath)).execute())
+                .closeConnection()
+                .getResultAsInt() == 1;
     }
 
     /**
@@ -41,9 +56,14 @@ public final class PictureManager {
      *             picture, the query will not be executed.
      * @param account the account that is performing this operation. If this account has not the permissions
      *                to accomplish the operation, the query will not be executed.
+     * @return {@code true} if the picture is removed successfully, {@code false} otherwise.
      */
-    public static void removePicture(final @NonNull String path, final @NonNull String account) {
-        throw new UnsupportedOperationException("Not implemented yet");
+    public static boolean removePicture(final @NonNull String path, final @NonNull String account) {
+        return new QueryBuilder().createConnection()
+                .queryAction(db -> db.deleteFrom(PICTURES)
+                        .where(PICTURES.PATH.eq(path)).execute())
+                .closeConnection()
+                .getResultAsInt() == 1;
     }
 
 }
