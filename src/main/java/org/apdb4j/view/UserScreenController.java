cd1b3c6be634d5882bdf10c8c354b7cfba049388
Raffaele Francesco Marrazzo <raffaele.marrazzo@studio.unibo.it>
2023-12-16 19:45:35 +0100

    wip: implemented basic user screen

---
+++ src/main/java/org/apdb4j/view/UserScreenController.java
@@ -0,0 +1,98 @@
+package org.apdb4j.view;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.geometry.Orientation;
+import javafx.scene.control.*;
+import javafx.scene.image.*;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * FXML controller for the user UI.
+ */
+public class UserScreenController implements Initializable {
+
+    private static final double BACKGROUND_IMAGE_OPACITY = 0.4;
+    private static final double ACCOUNT_IMAGE_HEIGHT = 50;
+    private static final double ACCOUNT_IMAGE_WIDTH = 50;
+    private static final double ACCOUNT_SCROLLABLE_MENU_MAX_HEIGHT = 100;
+    @FXML
+    private Label accountLabel;
+    @FXML
+    private BorderPane pane;
+    @FXML
+    private Label welcomeLabel;
+    private boolean isPersonalAreaOpen;
+    private final ToolBar accountInfo = new ToolBar();
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void initialize(final URL location, final ResourceBundle resources) {
+        final Background background = new Background(new BackgroundImage(makeImageTransparent(new Image("img/park.jpg"),
+                BACKGROUND_IMAGE_OPACITY),
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.DEFAULT,
+                new BackgroundSize(BackgroundSize.AUTO,
+                        BackgroundSize.AUTO,
+                        false,
+                        false,
+                        true,
+                        true)));
+        final ImageView accountImage = new ImageView(new Image("img/account.png"));
+        accountImage.setFitHeight(ACCOUNT_IMAGE_HEIGHT);
+        accountImage.setFitWidth(ACCOUNT_IMAGE_WIDTH);
+        accountLabel.setGraphic(accountImage);
+        pane.setBackground(background);
+        // ToolBar initalization
+        final Hyperlink accountPurchases = new Hyperlink("My purchases");
+        final Hyperlink accountPersonalData = new Hyperlink("My personal data");
+        final Button logOut = new Button("Log out");
+        accountInfo.getItems().add(accountPurchases);
+        accountInfo.getItems().add(accountPersonalData);
+        accountInfo.getItems().add(logOut);
+        for (final var accountInfoItem : accountInfo.getItems()) {
+            accountInfoItem.setFocusTraversable(false);
+        }
+        accountInfo.setOrientation(Orientation.VERTICAL);
+        accountInfo.setMaxHeight(ACCOUNT_SCROLLABLE_MENU_MAX_HEIGHT);
+    }
+
+    /**
+     * Opens the user's personal area.
+     * @param event the event.
+     */
+    @FXML
+    void openPersonalArea(final MouseEvent event) {
+        if (!isPersonalAreaOpen) {
+            pane.setRight(accountInfo);
+            isPersonalAreaOpen = true;
+        } else {
+            pane.getChildren().remove(accountInfo);
+            isPersonalAreaOpen = false;
+        }
+    }
+
+    private Image makeImageTransparent(final Image originalImage, final double opacity) {
+        final int width = (int) originalImage.getWidth();
+        final int height = (int) originalImage.getHeight();
+
+        final WritableImage result = new WritableImage(width, height);
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                final Color color = originalImage.getPixelReader().getColor(x, y);
+                result.getPixelWriter().setColor(x, y,
+                        new Color(color.getRed(), color.getGreen(), color.getBlue(), opacity));
+            }
+        }
+
+        return result;
+    }
+}
