09f35bd710e386dcea8d56481a4c23c0803b6db1
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2023-12-16 15:01:35 +0100

    Added add-ticket screen fxml with controller, loaded from TicketSelectorController

---
+++ src/main/java/org/apdb4j/view/AddTicketController.java
@@ -0,0 +1,56 @@
+package org.apdb4j.view;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * The FXML controller for the add ticket screen.
+ */
+public class AddTicketController implements Initializable {
+
+    @FXML
+    private ChoiceBox<?> categoryChoiceBox;
+    @FXML
+    private GridPane gridPane;
+    @FXML
+    private TextField ownerIDField;
+    @FXML
+    private DatePicker validOnDatePicker;
+    @FXML
+    private DatePicker validUntilDatePicker;
+    @FXML
+    private Button acceptAndCloseBtn;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void initialize(final URL location, final ResourceBundle resources) {
+        Platform.runLater(() -> {
+            final var stage = safeCastToStage(gridPane.getScene().getWindow());
+            stage.setResizable(false);
+        });
+    }
+
+    private Stage safeCastToStage(final Window window) {
+        Stage stage;
+        if (window instanceof Stage) {
+            stage = (Stage) window;
+        } else {
+            throw new IllegalStateException("Failed cast: the given window is not a Stage instance");
+        }
+        return stage;
+    }
+
+}
