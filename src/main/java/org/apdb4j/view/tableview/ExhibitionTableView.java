35f92567b05867252f632d32ccfb73cc617671a5
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2023-12-18 21:26:20 +0100

    Added attraction, ride and exhibition tableview classes

---
+++ src/main/java/org/apdb4j/view/tableview/ExhibitionTableView.java
@@ -0,0 +1,9 @@
+package org.apdb4j.view.tableview;
+
+/**
+ * An exhibition attraction representation used by the table view in the GUI.
+ * @see AttractionTableView
+ * @see javafx.scene.control.TableView
+ */
+public class ExhibitionTableView implements AttractionTableView {
+}
---
+++ src/main/java/org/apdb4j/view/tableview/RideTableView.java
@@ -0,0 +1,9 @@
+package org.apdb4j.view.tableview;
+
+/**
+ * A ride attraction representation used by the table view in the GUI.
+ * @see AttractionTableView
+ * @see javafx.scene.control.TableView
+ */
+public class RideTableView implements AttractionTableView {
+}
