2ab4e14dbfb285c499984de9eab2435a67e1b2e7
Sofia Cipollone <sofia.cipollone5@gmail.com>
2023-12-18 21:26:20 +0100

    Added attraction, ride and exhibition tableview classes

---
+++ src/main/java/org/apdb4j/view/tableview/AttractionTableView.java
@@ -0,0 +1,8 @@
+package org.apdb4j.view.tableview;
+
+/**
+ * An attraction representation used by the table view in the GUI.
+ * @see javafx.scene.control.TableView
+ */
+public interface AttractionTableView {
+}
