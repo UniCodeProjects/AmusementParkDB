5ea07d7bdbc9034982d023b2aada3acef8f26f34
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2023-12-19 18:46:43 +0100

    Added add-ride fxml, base controller and loading in StaffScreenController

---
+++ src/main/java/org/apdb4j/view/AddRideController.java
@@ -0,0 +1,88 @@
+package org.apdb4j.view;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Spinner;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+import javafx.stage.Window;
+import lombok.Setter;
+import org.apdb4j.view.tableview.RideTableView;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ * The FXML controller for the add ride screen.
+ */
+public class AddRideController implements Initializable {
+
+    @FXML
+    private GridPane gridPane;
+    @FXML
+    private TextField nameField;
+    @FXML
+    private TextField openingHourField;
+    @FXML
+    private TextField openingMinuteField;
+    @FXML
+    private TextField closingHourField;
+    @FXML
+    private TextField closingMinuteField;
+    @FXML
+    private ChoiceBox<String> typeChoiceBox;
+    @FXML
+    private ChoiceBox<String> intensityChoiceBox;
+    @FXML
+    private TextField durationField;
+    @FXML
+    private Spinner<Integer> maxSeatsSpinner;
+    @FXML
+    private TextField descriptionField;
+    @FXML
+    private Spinner<Integer> minHeightSpinner;
+    @FXML
+    private Spinner<Integer> maxHeightSpinner;
+    @FXML
+    private Spinner<Double> minWeightSpinner;
+    @FXML
+    private Spinner<Double> maxWeightSpinner;
+    @FXML
+    private ChoiceBox<String> statusChoiceBox;
+    @FXML
+    private Button acceptAndCloseBtn;
+    @Setter
+    private static boolean editMode;
+    @Setter
+    private static RideTableView ride;
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void initialize(final URL location, final ResourceBundle resources) {
+        Platform.runLater(() -> {
+            final var stage = safeCastToStage(gridPane.getScene().getWindow());
+            stage.setResizable(false);
+            if (editMode) {
+                // TODO
+                throw new UnsupportedOperationException();
+            }
+        });
+    }
+
+    private Stage safeCastToStage(final Window window) {
+        Stage stage;
+        if (window instanceof Stage) {
+            stage = (Stage) window;
+        } else {
+            throw new IllegalStateException("Failed cast: the given window is not a Stage instance");
+        }
+        return stage;
+    }
+
+}
