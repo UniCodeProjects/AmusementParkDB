09e50bcad4c23c0bbddc20f2a23b838fb8e404ae
Sofia Cipollone <sofia.cipollone5@gmail.com>
2023-12-22 01:00:40 +0100

    Added ReviewTableView class

---
+++ src/main/java/org/apdb4j/view/tableview/ReviewTableView.java
@@ -0,0 +1,61 @@
+package org.apdb4j.view.tableview;
+
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+import lombok.EqualsAndHashCode;
+import lombok.NonNull;
+import lombok.ToString;
+
+/**
+ * A review representation used by the table view in the GUI.
+ * @see javafx.scene.control.TableView
+ */
+@ToString
+@EqualsAndHashCode
+public class ReviewTableView {
+
+    private final StringProperty serviceID;
+    private final DoubleProperty rating;
+    private final StringProperty description;
+
+    /**
+     * Default constructor.
+     * @param serviceID the park service ID related to this review
+     * @param rating the review rating
+     * @param description the review description
+     */
+    public ReviewTableView(final @NonNull String serviceID,
+                           final double rating,
+                           final @NonNull String description) {
+        this.serviceID = new SimpleStringProperty(serviceID);
+        this.rating = new SimpleDoubleProperty(rating);
+        this.description = new SimpleStringProperty(description);
+    }
+
+    /**
+     * Returns the park service ID related to this review.
+     * @return the park service ID related to this review
+     */
+    public String getServiceID() {
+        return serviceID.get();
+    }
+
+    /**
+     * Returns the review rating.
+     * @return the review rating
+     */
+    public double getRating() {
+        return rating.get();
+    }
+
+    /**
+     * Returns the review description.
+     * @return the review description
+     */
+    public String getDescription() {
+        return description.get();
+    }
+
+}
