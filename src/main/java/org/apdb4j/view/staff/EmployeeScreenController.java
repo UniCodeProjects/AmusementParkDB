7421754eea722f682972c69f474ae2b663a297eb
Sofia Cipollone <sofia.cipollone5@gmail.com>
2023-12-26 12:45:29 +0100

    Added onAccept method in EmployeeScreenController

--- src/main/java/org/apdb4j/view/staff/EmployeeScreenController.java
+++ src/main/java/org/apdb4j/view/staff/EmployeeScreenController.java
@@ -1,16 +1,19 @@
 package org.apdb4j.view.staff;
 
 import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.Spinner;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.GridPane;
 import lombok.Setter;
+import org.apdb4j.controllers.EmployeeControllerImpl;
 import org.apdb4j.view.PopupInitializer;
 import org.apdb4j.view.staff.tableview.EmployeeTableItem;
 
@@ -52,6 +55,8 @@ public class EmployeeScreenController extends PopupInitializer {
     private static boolean editMode;
     @Setter
     private static EmployeeTableItem employee;
+    @Setter
+    private static TableView<EmployeeTableItem> tableView;
 
     /**
      * Default constructor.
@@ -61,6 +66,29 @@ public class EmployeeScreenController extends PopupInitializer {
             super.setStage(gridPane.getScene().getWindow());
             super.setRoot(gridPane.getScene().getRoot());
         });
+    }
+
+    /**
+     * Adds the new employee into the DB and displays the new row in the table view.
+     * @param event the event
+     */
+    @FXML
+    void onAccept(final ActionEvent event) {
+        final var employee = new EmployeeTableItem(
+                "B-002",    // TODO: use the ID generator.
+                nationalIDField.getText(),
+                nameField.getText(),
+                surnameField.getText(),
+                dobPicker.getValue(),
+                birthplaceField.getText(),
+                genderChoiceBox.getValue(),
+                roleField.getText(),
+                adminRadioBtn.isSelected(),
+                salarySpinner.getValue(),
+                emailField.getText()
+        );
+        Platform.runLater(() -> tableView.getItems().add(new EmployeeControllerImpl().addData(employee)));
+        gridPane.getScene().getWindow().hide();
     }
 
     /**
