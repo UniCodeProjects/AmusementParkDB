8c47b991127ca7b6617c08a7772af2f24db649e5
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2023-12-26 13:57:26 +0100

    Using radio btns for gender selection in employee form

--- src/main/java/org/apdb4j/view/staff/EmployeeScreenController.java
+++ src/main/java/org/apdb4j/view/staff/EmployeeScreenController.java
@@ -4,7 +4,6 @@ import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.Spinner;
@@ -12,6 +11,7 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.layout.GridPane;
+import lombok.NonNull;
 import lombok.Setter;
 import org.apdb4j.controllers.EmployeeControllerImpl;
 import org.apdb4j.view.PopupInitializer;
@@ -35,7 +35,13 @@ public class EmployeeScreenController extends PopupInitializer {
     @FXML
     private TextField birthplaceField;
     @FXML
-    private ChoiceBox<String> genderChoiceBox;
+    private ToggleGroup genderToggleGroup;
+    @FXML
+    private RadioButton maleRadioBtn;
+    @FXML
+    private RadioButton femaleRadioBtn;
+    @FXML
+    private RadioButton otherRadioBtn;
     @FXML
     private TextField roleField;
     @FXML
@@ -75,13 +81,13 @@ public class EmployeeScreenController extends PopupInitializer {
     @FXML
     void onAccept(final ActionEvent event) {
         final var employee = new EmployeeTableItem(
-                "B-002",    // TODO: use the ID generator.
+                "E-000",    // TODO: use the ID generator.
                 nationalIDField.getText(),
                 nameField.getText(),
                 surnameField.getText(),
                 dobPicker.getValue(),
                 birthplaceField.getText(),
-                genderChoiceBox.getValue(),
+                getGenderString(),
                 roleField.getText(),
                 adminRadioBtn.isSelected(),
                 salarySpinner.getValue(),
@@ -104,12 +110,31 @@ public class EmployeeScreenController extends PopupInitializer {
         surnameField.setText(employee.getSurname());
         dobPicker.setValue(employee.getDob());
         birthplaceField.setText(employee.getBirthplace());
-        genderChoiceBox.setValue(employee.getGender());
+        setGenderToggle(employee.getGender());
         roleField.setText(employee.getRole());
         adminRadioBtn.setSelected(employee.isAdmin());
         employeeRadioBtn.setSelected(!employee.isAdmin());
         salarySpinner.getValueFactory().setValue(employee.getSalary());
         emailField.setText(employee.getEmail());
+    }
+
+    private @NonNull String getGenderString() {
+        if (maleRadioBtn.isSelected()) {
+            return "M";
+        } else if (femaleRadioBtn.isSelected()) {
+            return "F";
+        } else {
+            return "O";
+        }
+    }
+
+    private void setGenderToggle(final @NonNull String gender) {
+        switch (gender) {
+            case "M" -> maleRadioBtn.setSelected(true);
+            case "F" -> femaleRadioBtn.setSelected(true);
+            case "O" -> otherRadioBtn.setSelected(true);
+            default -> throw new IllegalStateException(gender + " is invalid.");
+        }
     }
 
 }
