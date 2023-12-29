9b11913120680ef1294b9fad1fc9ea8bc9fc2375
Sofia Cipollone <sofia.cipollone5@gmail.com>
2023-12-29 01:09:20 +0100

    Added method to retrieve the data of fired employees, refactoring

--- src/main/java/org/apdb4j/controllers/EmployeeController.java
+++ src/main/java/org/apdb4j/controllers/EmployeeController.java
@@ -2,6 +2,8 @@ package org.apdb4j.controllers;
 
 import org.apdb4j.view.staff.tableview.TableItem;
 
+import java.util.Collection;
+
 /**
  * Specialised controller for handling employees.
  */
@@ -14,5 +16,13 @@ public interface EmployeeController extends AdministrationController {
      * @return the fired employee
      */
     <T extends TableItem> T fire(T employeeItem);
+
+    /**
+     * Retrieves all the table items of fired employees.
+     * @param <T> the type of the {@code TableItem}
+     * @return a collection of fired employees table items
+     * @see TableItem
+     */
+    <T extends TableItem> Collection<T> getFiredData();
 
 }
