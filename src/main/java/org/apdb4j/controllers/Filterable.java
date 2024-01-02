e57c195c84f606c503ed1de28d19a343904c5414
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2024-01-02 17:54:51 +0100

    Added Filterable interface for controllers

---
+++ src/main/java/org/apdb4j/controllers/Filterable.java
@@ -0,0 +1,20 @@
+package org.apdb4j.controllers;
+
+import org.apdb4j.view.staff.tableview.TableItem;
+
+import java.util.Collection;
+
+/**
+ * Represents a controller that can be filtered in some way.
+ */
+public interface Filterable {
+
+    /**
+     * Filters all the data in this table view based on the given string.
+     * @param s the string to filter on
+     * @param <T> the type of the {@code TableItem}
+     * @return the filtered data
+     */
+    <T extends TableItem> Collection<T> filter(String s);
+
+}
