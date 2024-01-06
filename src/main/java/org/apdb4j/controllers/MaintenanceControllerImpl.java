48d018ce67761478ac4544671feee10d93904161
Sofia Cipollone <sofia.cipollone5@gmail.com>
2024-01-07 00:32:32 +0100

    wip: Added maintenance controller

---
+++ src/main/java/org/apdb4j/controllers/MaintenanceControllerImpl.java
@@ -0,0 +1,83 @@
+package org.apdb4j.controllers;
+
+import lombok.NonNull;
+import org.apdb4j.util.QueryBuilder;
+import org.apdb4j.view.staff.tableview.MaintenanceTableItem;
+import org.apdb4j.view.staff.tableview.TableItem;
+import org.jooq.Record;
+import org.jooq.Result;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Optional;
+
+import static org.apdb4j.db.Tables.MAINTENANCES;
+import static org.apdb4j.db.Tables.RESPONSIBILITIES;
+
+/**
+ * An implementation of a maintenance controller.
+ */
+public class MaintenanceControllerImpl implements MaintenanceController {
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public <T extends TableItem> Collection<T> getData() {
+        final Result<Record> result = new QueryBuilder().createConnection()
+                .queryAction(db -> db.select(MAINTENANCES.asterisk(),
+                                RESPONSIBILITIES.asterisk().except(RESPONSIBILITIES.FACILITYID, RESPONSIBILITIES.DATE))
+                        .from(MAINTENANCES)
+                        .join(RESPONSIBILITIES)
+                        .onKey()
+                        .fetch())
+                .closeConnection()
+                .getResultAsRecords();
+        return extractMaintenanceData(result);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public <T extends TableItem> T addData(final T item) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public <T extends TableItem> T editData(final T item) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public <T extends TableItem> Collection<T> filter(final String s) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public @NonNull Optional<String> getErrorMessage() {
+        return Optional.empty();
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T extends TableItem> Collection<T> extractMaintenanceData(final Result<Record> result) {
+        final List<T> data = new ArrayList<>();
+        result.forEach(record -> data.add((T) new MaintenanceTableItem(record.get(MAINTENANCES.FACILITYID),
+                record.get(MAINTENANCES.PRICE).doubleValue(),
+                record.get(MAINTENANCES.DESCRIPTION),
+                record.get(MAINTENANCES.DATE),
+                record.get(RESPONSIBILITIES.EMPLOYEENID))));
+        return data;
+    }
+
+}
