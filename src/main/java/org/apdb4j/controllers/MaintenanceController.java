108598e20529f72d8a98da56c5831a28573255cd
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2024-01-07 00:32:32 +0100

    wip: Added maintenance controller

---
+++ src/main/java/org/apdb4j/controllers/MaintenanceController.java
@@ -0,0 +1,7 @@
+package org.apdb4j.controllers;
+
+/**
+ * An administration controller specifically designed for managing maintenances.
+ */
+public interface MaintenanceController extends AdministrationController, Filterable {
+}
