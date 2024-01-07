ce24a749ce7a59a50cdebe229f8601e8d5356929
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2024-01-07 13:36:54 +0100

    Implemented search for maintenance tab

--- src/main/java/org/apdb4j/controllers/MaintenanceControllerImpl.java
+++ src/main/java/org/apdb4j/controllers/MaintenanceControllerImpl.java
@@ -4,8 +4,10 @@ import lombok.NonNull;
 import org.apdb4j.util.QueryBuilder;
 import org.apdb4j.view.staff.tableview.MaintenanceTableItem;
 import org.apdb4j.view.staff.tableview.TableItem;
+import org.jooq.Condition;
 import org.jooq.Record;
 import org.jooq.Result;
+import org.jooq.impl.DSL;
 
 import java.util.ArrayList;
 import java.util.Collection;
@@ -25,16 +27,7 @@ public class MaintenanceControllerImpl implements MaintenanceController {
      */
     @Override
     public <T extends TableItem> Collection<T> getData() {
-        final Result<Record> result = new QueryBuilder().createConnection()
-                .queryAction(db -> db.select(MAINTENANCES.asterisk(),
-                                RESPONSIBILITIES.asterisk().except(RESPONSIBILITIES.FACILITYID, RESPONSIBILITIES.DATE))
-                        .from(MAINTENANCES)
-                        .join(RESPONSIBILITIES)
-                        .onKey()
-                        .fetch())
-                .closeConnection()
-                .getResultAsRecords();
-        return extractMaintenanceData(result);
+        return extractMaintenanceData(searchQuery(DSL.condition(true)));
     }
 
     /**
@@ -57,8 +50,8 @@ public class MaintenanceControllerImpl implements MaintenanceController {
      * {@inheritDoc}
      */
     @Override
-    public <T extends TableItem> Collection<T> filter(final String s) {
-        throw new UnsupportedOperationException();
+    public <T extends TableItem> Collection<T> filter(final String facilityID) {
+        return extractMaintenanceData(searchQuery(MAINTENANCES.FACILITYID.containsIgnoreCase(facilityID)));
     }
 
     /**
@@ -67,6 +60,19 @@ public class MaintenanceControllerImpl implements MaintenanceController {
     @Override
     public @NonNull Optional<String> getErrorMessage() {
         return Optional.empty();
+    }
+
+    private Result<Record> searchQuery(final Condition condition) {
+        return new QueryBuilder().createConnection()
+                .queryAction(db -> db.select(MAINTENANCES.asterisk(),
+                                RESPONSIBILITIES.asterisk().except(RESPONSIBILITIES.FACILITYID, RESPONSIBILITIES.DATE))
+                        .from(MAINTENANCES)
+                        .join(RESPONSIBILITIES)
+                        .onKey()
+                        .and(condition)
+                        .fetch())
+                .closeConnection()
+                .getResultAsRecords();
     }
 
     @SuppressWarnings("unchecked")
