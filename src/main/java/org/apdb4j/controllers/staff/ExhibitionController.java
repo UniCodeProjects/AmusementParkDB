7a21ec60220dc18e86e67ef6ce9e1666267b2c6f
Sofia Cipollone <sofia.cipollone5@gmail.com>
2024-01-12 15:15:13 +0100

    Added attractions controllers

---
+++ src/main/java/org/apdb4j/controllers/staff/ExhibitionController.java
@@ -0,0 +1,9 @@
+package org.apdb4j.controllers.staff;
+
+import org.apdb4j.controllers.Filterable;
+
+/**
+ * An administration controller specifically used for exhibitions.
+ */
+public interface ExhibitionController extends AdministrationController, Filterable {
+}
---
+++ src/main/java/org/apdb4j/controllers/staff/RideController.java
@@ -0,0 +1,9 @@
+package org.apdb4j.controllers.staff;
+
+import org.apdb4j.controllers.Filterable;
+
+/**
+ * An administration controller specifically used for rides.
+ */
+public interface RideController extends AdministrationController, Filterable {
+}
