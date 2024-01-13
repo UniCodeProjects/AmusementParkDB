c89a6e6b7479137852cbb1143f26ce1ec0b3ee29
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2024-01-13 13:42:36 +0100

    wip: implemented addData, editData to fix in ride controller

--- src/main/java/org/apdb4j/controllers/staff/RideControllerImpl.java
+++ src/main/java/org/apdb4j/controllers/staff/RideControllerImpl.java
@@ -1,15 +1,20 @@
 package org.apdb4j.controllers.staff;
 
 import lombok.NonNull;
+import org.apdb4j.core.managers.RideManager;
 import org.apdb4j.util.QueryBuilder;
 import org.apdb4j.view.staff.tableview.RideTableItem;
 import org.apdb4j.view.staff.tableview.TableItem;
 import org.jooq.Condition;
 import org.jooq.Record;
 import org.jooq.Result;
+import org.jooq.exception.DataAccessException;
 import org.jooq.impl.DSL;
+import org.jooq.types.UInteger;
 
-import java.sql.SQLException;
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.LocalTime;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
@@ -22,8 +27,11 @@ import static org.apdb4j.db.Tables.*;
  */
 public class RideControllerImpl implements RideController {
 
+    private String errorMessage;
+
     /**
      * {@inheritDoc}
+     * @throws org.jooq.exception.DataAccessException if query fails
      */
     @Override
     public <T extends TableItem> Collection<T> getData() {
@@ -32,22 +40,75 @@ public class RideControllerImpl implements RideController {
 
     /**
      * {@inheritDoc}
+     * @throws org.jooq.exception.DataAccessException if query fails
      */
     @Override
-    public <T extends TableItem> T addData(final T item) throws SQLException {
-        return null;
+    public <T extends TableItem> T addData(final T item) {
+        final RideTableItem ride = (RideTableItem) item;
+        final Duration rideDuration = Duration.ofMinutes(ride.getDuration());
+        final boolean successfulQuery = RideManager.addNewRide(ride.getId(),
+                ride.getName(),
+                ride.getOpeningTime(),
+                ride.getClosingTime(),
+                ride.getType(),
+                ride.getIntensity(),
+                LocalTime.of(rideDuration.toHoursPart(), rideDuration.toMinutesPart()),
+                ride.getMaxSeats(),
+                ride.getDescription(),
+                ride.getMinHeight(),
+                ride.getMaxHeight(),
+                Math.toIntExact(Math.round(ride.getMinWeight())),
+                Math.toIntExact(Math.round(ride.getMaxWeight())),
+                ride.getStatus().charAt(0),
+                "");
+        if (!successfulQuery) {
+            errorMessage = "Could not add ride.";
+            throw new DataAccessException(errorMessage);
+        }
+        return item;
     }
 
     /**
      * {@inheritDoc}
+     * @throws org.jooq.exception.DataAccessException if query fails
      */
     @Override
     public <T extends TableItem> T editData(final T item) {
-        return null;
+        final RideTableItem ride = (RideTableItem) item;
+        final Duration rideDuration = Duration.ofMinutes(ride.getDuration());
+        // fixme
+        new QueryBuilder().createConnection()
+                .queryAction(db -> db.update(RIDES
+                                .join(RIDE_DETAILS)
+                                .on(RIDE_DETAILS.RIDEID.eq(RIDES.RIDEID))
+                                .join(PARK_SERVICES)
+                                .on(PARK_SERVICES.PARKSERVICEID.eq(RIDES.RIDEID))
+                                .join(FACILITIES)
+                                .on(FACILITIES.FACILITYID.eq(RIDES.RIDEID)))
+                        .set(RIDES.RIDEID, ride.getId())
+                        .set(PARK_SERVICES.NAME, ride.getName())
+                        .set(FACILITIES.OPENINGTIME, ride.getOpeningTime())
+                        .set(FACILITIES.CLOSINGTIME, ride.getClosingTime())
+                        .set(PARK_SERVICES.TYPE, ride.getType())
+                        .set(RIDES.INTENSITY, ride.getIntensity())
+                        .set(RIDES.DURATION, LocalTime.of(rideDuration.toHoursPart(), rideDuration.toMinutesPart()))
+                        .set(RIDES.MAXSEATS, ride.getMaxSeats())
+                        .set(PARK_SERVICES.DESCRIPTION, ride.getDescription())
+                        .set(RIDES.MINHEIGHT, UInteger.valueOf(ride.getMinHeight()))
+                        .set(RIDES.MAXHEIGHT, UInteger.valueOf(ride.getMaxHeight()))
+                        .set(RIDES.MINWEIGHT, UInteger.valueOf(Math.round(ride.getMinWeight())))
+                        .set(RIDES.MAXWEIGHT, UInteger.valueOf(Math.round(ride.getMaxWeight())))
+                        .set(RIDE_DETAILS.STATUS, ride.getStatus())
+                        .set(PARK_SERVICES.AVGRATING, BigDecimal.valueOf(ride.getAverageRating()))
+                        .set(PARK_SERVICES.NUMREVIEWS, UInteger.valueOf(ride.getRatings()))
+                        .execute())
+                .closeConnection();
+        return item;
     }
 
     /**
      * {@inheritDoc}
+     * @throws org.jooq.exception.DataAccessException if query fails
      */
     @Override
     public <T extends TableItem> Collection<T> filter(final String attractionName) {
@@ -59,7 +120,7 @@ public class RideControllerImpl implements RideController {
      */
     @Override
     public @NonNull Optional<String> getErrorMessage() {
-        return Optional.empty();
+        return Optional.ofNullable(errorMessage);
     }
 
     private Result<Record> searchQuery(final Condition condition) {
