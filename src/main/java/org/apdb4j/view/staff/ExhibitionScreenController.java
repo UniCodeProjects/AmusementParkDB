2ce0c793e44797c7a8a0d8c4c0a520f8bd4b5795
Francesco Cipollone <francesco.cipollone@studio.unibo.it>
2024-01-14 17:27:03 +0100

    Loading types and disabling specific nodes during add mode in exhibition screen controller

--- src/main/java/org/apdb4j/view/staff/ExhibitionScreenController.java
+++ src/main/java/org/apdb4j/view/staff/ExhibitionScreenController.java
@@ -3,15 +3,19 @@ package org.apdb4j.view.staff;
 import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
 import javafx.scene.control.Spinner;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import lombok.Setter;
+import org.apdb4j.controllers.staff.ExhibitionControllerImpl;
 import org.apdb4j.view.PopupInitializer;
 import org.apdb4j.view.staff.tableview.ExhibitionTableItem;
+
+import java.util.List;
 
 /**
  * The FXML controller for the exhibition screen.
@@ -23,17 +27,27 @@ public class ExhibitionScreenController extends PopupInitializer {
     @FXML
     private TextField nameField;
     @FXML
-    private ChoiceBox<String> typeChoiceBox;
+    private ComboBox<String> typeComboBox;
     @FXML
     private TextArea descriptionTextArea;
     @FXML
+    private Label dateLabel;
+    @FXML
     private DatePicker datePicker;
+    @FXML
+    private Label timeLabel;
+    @FXML
+    private Label timeLabel2;
     @FXML
     private Spinner<Integer> timeHourSpinner;
     @FXML
     private Spinner<Integer> timeMinuteSpinner;
     @FXML
+    private Label maxSeatsLabel;
+    @FXML
     private Spinner<Integer> maxSeatsSpinner;
+    @FXML
+    private Label spectatorsLabel;
     @FXML
     private Spinner<Integer> spectatorsSpinner;
     @FXML
@@ -58,11 +72,22 @@ public class ExhibitionScreenController extends PopupInitializer {
      */
     @Override
     protected void customInit() {
+        typeComboBox.getItems().addAll(new ExhibitionControllerImpl().getExistingTypes());
         if (!editMode) {
+            List.of(dateLabel,
+                    datePicker,
+                    timeLabel,
+                    timeLabel2,
+                    timeHourSpinner,
+                    timeMinuteSpinner,
+                    maxSeatsLabel,
+                    maxSeatsSpinner,
+                    spectatorsLabel,
+                    spectatorsSpinner).forEach(control -> control.setDisable(true));
             return;
         }
         nameField.setText(exhibition.getName());
-        typeChoiceBox.setValue(exhibition.getType());
+        typeComboBox.setValue(exhibition.getType());
         descriptionTextArea.setText(exhibition.getDescription());
         datePicker.setValue(exhibition.getDate());
         timeHourSpinner.getValueFactory().setValue(exhibition.getTime().getHour());
