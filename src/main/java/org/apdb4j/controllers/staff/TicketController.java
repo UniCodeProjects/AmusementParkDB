7e631166c2d44e14559d23f20b9f9351a07cc00d
Sofia Cipollone <sofia.cipollone5@gmail.com>
2024-01-18 15:46:52 +0100

    Added new methods in TicketController, wip: implemented filters and add methods

--- src/main/java/org/apdb4j/controllers/staff/TicketController.java
+++ src/main/java/org/apdb4j/controllers/staff/TicketController.java
@@ -1,9 +1,76 @@
 package org.apdb4j.controllers.staff;
 
 import org.apdb4j.controllers.Filterable;
+import org.apdb4j.view.staff.tableview.TicketTableItem;
+
+import java.time.LocalDate;
+import java.util.Collection;
 
 /**
  * An administration controller specifically used for tickets.
  */
 public interface TicketController extends AdministrationController, Filterable {
+
+    /**
+     * Adds a new ticket type to the DB.
+     * @param ticket the ticket
+     * @param <T> the {@code TicketTableItem} type
+     * @return the new ticket table item
+     */
+    <T extends TicketTableItem> T addTicketType(T ticket);
+
+    /**
+     * Edits a ticket type.
+     * @param ticket the ticket
+     * @param <T> the {@code TicketTableItem} type
+     * @return the edited ticket table item
+     */
+    <T extends TicketTableItem> T editTicketType(T ticket);
+
+    /**
+     * Adds a ticket price list to the DB.
+     * @param ticket the ticket
+     * @param <T> the {@code TicketTableItem} type
+     * @return the new ticket table item
+     */
+    <T extends TicketTableItem> T addPriceList(T ticket);
+
+    /**
+     * Edits a ticket price list.
+     * @param ticket the ticket
+     * @param <T> the {@code TicketTableItem} type
+     * @return the edited ticket table item
+     */
+    <T extends TicketTableItem> T editPriceList(T ticket);
+
+    /**
+     * Filters tickets by purchase date.
+     * @param date the date
+     * @param <T> the {@code TicketTableItem} type
+     * @return a collection of filtered ticket table items
+     */
+    <T extends TicketTableItem> Collection<T> filterByPurchaseDate(LocalDate date);
+
+    /**
+     * Filters tickets by punch date.
+     * @param date the date
+     * @param <T> the {@code TicketTableItem} type
+     * @return a collection of filtered ticket table items
+     */
+    <T extends TicketTableItem> Collection<T> filterByPunchDate(LocalDate date);
+
+    /**
+     * Filters tickets by the 'single day' ticket type.
+     * @param <T> the {@code TicketTableItem} type
+     * @return a collection of filtered ticket table items
+     */
+    <T extends TicketTableItem> Collection<T> filterBySingleDayTicket();
+
+    /**
+     * Filters tickets by the 'season' ticket type.
+     * @param <T> the {@code TicketTableItem} type
+     * @return a collection of filtered ticket table items
+     */
+    <T extends TicketTableItem> Collection<T> filterBySeasonTicket();
+
 }
--- src/main/java/org/apdb4j/controllers/staff/TicketControllerImpl.java
+++ src/main/java/org/apdb4j/controllers/staff/TicketControllerImpl.java
@@ -1,14 +1,17 @@
 package org.apdb4j.controllers.staff;
 
 import lombok.NonNull;
+import org.apdb4j.core.managers.TicketManager;
 import org.apdb4j.util.QueryBuilder;
 import org.apdb4j.view.staff.tableview.TableItem;
 import org.apdb4j.view.staff.tableview.TicketTableItem;
 import org.jooq.Condition;
 import org.jooq.Record;
 import org.jooq.Result;
+import org.jooq.exception.DataAccessException;
 import org.jooq.impl.DSL;
 
+import java.time.LocalDate;
 import java.time.Year;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -37,7 +40,16 @@ public class TicketControllerImpl implements TicketController {
      */
     @Override
     public <T extends TableItem> T addData(final T item) {
-        return null;
+        final TicketTableItem ticket = (TicketTableItem) item;
+        final boolean queryResult = TicketManager.addNewTicket(ticket.getValidOn(),
+                ticket.getValidUntil(),
+                ticket.getOwnerID(),
+                ticket.getCategory(),
+                "");
+        if (!queryResult) {
+            throw new DataAccessException("Something went wrong while adding a new ticket.");
+        }
+        return item;
     }
 
     /**
@@ -46,7 +58,95 @@ public class TicketControllerImpl implements TicketController {
      */
     @Override
     public <T extends TableItem> T editData(final T item) {
-        return null;
+        // TODO
+        return item;
+    }
+
+    /**
+     * {@inheritDoc}
+     * @throws org.jooq.exception.DataAccessException if the query fails
+     */
+    @Override
+    public <T extends TicketTableItem> T addTicketType(final T ticket) {
+        final boolean queryResult = TicketManager.addNewTicketType(ticket.getType(),
+                ticket.getTypePrice(),
+                ticket.getYear().getValue(),
+                ticket.getCategory(),
+                ticket.getDuration(),
+                "");
+        if (!queryResult) {
+            throw new DataAccessException("Something went wrong while adding a new ticket type.");
+        }
+        return ticket;
+    }
+
+    /**
+     * {@inheritDoc}
+     * @throws org.jooq.exception.DataAccessException if the query fails
+     */
+    @Override
+    public <T extends TicketTableItem> T editTicketType(final T ticket) {
+        // TODO
+        return ticket;
+    }
+
+    /**
+     * {@inheritDoc}
+     * @throws org.jooq.exception.DataAccessException if the query fails
+     */
+    @Override
+    public <T extends TicketTableItem> T addPriceList(final T ticket) {
+        final boolean queryResult = TicketManager.addNewPriceList(ticket.getYear().getValue(), "");
+        if (!queryResult) {
+            throw new DataAccessException("Something went wrong while adding a new ticket price list.");
+        }
+        return ticket;
+    }
+
+    /**
+     * {@inheritDoc}
+     * @throws org.jooq.exception.DataAccessException if the query fails
+     */
+    @Override
+    public <T extends TicketTableItem> T editPriceList(final T ticket) {
+        // TODO
+        return ticket;
+    }
+
+    /**
+     * {@inheritDoc}
+     * @throws org.jooq.exception.DataAccessException if the query fails
+     */
+    @Override
+    public <T extends TicketTableItem> Collection<T> filterByPurchaseDate(final LocalDate date) {
+        return extractTicketData(searchQuery(TICKETS.PURCHASEDATE.eq(date)));
+    }
+
+    /**
+     * {@inheritDoc}
+     * @throws org.jooq.exception.DataAccessException if the query fails
+     */
+    @Override
+    public <T extends TicketTableItem> Collection<T> filterByPunchDate(final LocalDate date) {
+        return extractTicketData(searchQuery(VALIDATIONS.DATE.eq(date)));
+    }
+
+    /**
+     * {@inheritDoc}
+     * @throws org.jooq.exception.DataAccessException if the query fails
+     */
+    @Override
+    public <T extends TicketTableItem> Collection<T> filterBySingleDayTicket() {
+        return extractTicketData(searchQuery(ATTRIBUTIONS.TYPE.containsIgnoreCase("single day")));
+    }
+
+    /**
+     * {@inheritDoc}
+     * @throws org.jooq.exception.DataAccessException if the query fails
+     */
+    @Override
+    public <T extends TicketTableItem> Collection<T> filterBySeasonTicket() {
+        return extractTicketData(searchQuery(ATTRIBUTIONS.TYPE.containsIgnoreCase("season")));
     }
 
     /**
@@ -73,7 +173,11 @@ public class TicketControllerImpl implements TicketController {
                                 .join(ATTRIBUTIONS)
                                 .on(TICKETS.TICKETID.eq(ATTRIBUTIONS.TICKETID))
                                 .join(VALIDATIONS)
-                                .on(TICKETS.TICKETID.eq(VALIDATIONS.TICKETID)))
+                                .on(TICKETS.TICKETID.eq(VALIDATIONS.TICKETID))
+                                .join(TICKET_TYPES)
+                                .on(ATTRIBUTIONS.YEAR.eq(TICKET_TYPES.YEAR)
+                                        .and(ATTRIBUTIONS.TYPE.eq(TICKET_TYPES.TYPE))
+                                        .and(ATTRIBUTIONS.CATEGORY.eq(TICKET_TYPES.CATEGORY))))
                         .where(condition)
                         .fetch())
                 .closeConnection()
@@ -91,7 +195,9 @@ public class TicketControllerImpl implements TicketController {
                 record.get(TICKETS.OWNERID),
                 Year.of(record.get(ATTRIBUTIONS.YEAR)),
                 record.get(ATTRIBUTIONS.TYPE),
+                record.get(TICKET_TYPES.PRICE).doubleValue(),
                 record.get(ATTRIBUTIONS.CATEGORY),
+                record.get(TICKET_TYPES.DURATION),
                 record.get(VALIDATIONS.DATE))));
         return data;
     }
