a0a700026f8ae51b32da163023e28e6c52ddfb21
Raffaele Francesco Marrazzo <raffaele.marrazzo@studio.unibo.it>
2024-01-30 15:27:47 +0100

    Added private record in SessionManager that keeps track of the session information. Changed name to the SessionManager method that retrieves the singleton instance

--- src/main/java/org/apdb4j/controllers/SessionManager.java
+++ src/main/java/org/apdb4j/controllers/SessionManager.java
@@ -2,6 +2,7 @@ package org.apdb4j.controllers;
 
 import lombok.AccessLevel;
 import lombok.Getter;
+import org.apdb4j.core.managers.AccountManager;
 
 import java.util.Objects;
 
@@ -11,7 +12,7 @@ import java.util.Objects;
 public final class SessionManager {
     private static final SessionManager INSTANCE = new SessionManager();
     @Getter(AccessLevel.PUBLIC)
-    private String loggedAccount;
+    private SessionInfo session;
 
     private SessionManager() {
     }
@@ -20,18 +21,21 @@ public final class SessionManager {
      * Returns the singleton instance of this class.
      * @return the unique instance of this class.
      */
-    public static SessionManager getSession() {
+    public static SessionManager getSessionManager() {
         return INSTANCE;
     }
 
     /**
-     * If no account is logged in, keeps track of the provided {@code accountUsername}.
+     * If no account is logged in, creates a new session for the provided {@code accountUsername}.
      * Otherwise, if an account is already logged in, this method does nothing.
      * @param accountUsername the username of the account that is logged in.
      */
     public void login(final String accountUsername) {
-        if (Objects.isNull(loggedAccount)) {
-            loggedAccount = accountUsername;
+        if (Objects.isNull(session)) {
+            session = new SessionInfo(accountUsername, AccountManager.getPersonID(accountUsername));
         }
+    }
+
+    private record SessionInfo(String username, String personID) {
     }
 }
