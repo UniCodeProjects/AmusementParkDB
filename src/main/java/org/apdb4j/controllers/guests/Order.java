cfe8d58c5662f9398d6b7576850cef37ac9aedee
Raffaele Francesco Marrazzo <raffaele.marrazzo@studio.unibo.it>
2024-02-19 12:51:35 +0100

    Added name field in enum Order

--- src/main/java/org/apdb4j/controllers/guests/Order.java
+++ src/main/java/org/apdb4j/controllers/guests/Order.java
@@ -1,15 +1,26 @@
 package org.apdb4j.controllers.guests;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+
 /**
  * Enum that represents the sorting order.
  */
 public enum Order {
+
     /**
      * The ascending sorting order.
      */
-    ASCENDING,
+    ASCENDING("ascending"),
     /**
      * The descending sorting order.
      */
-    DESCENDING;
+    DESCENDING("descending");
+
+    @Getter(AccessLevel.PACKAGE)
+    private final String name;
+
+    Order(final String name) {
+        this.name = name;
+    }
 }
