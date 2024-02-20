41b0e58239c5e048cf8bd9e9df0641f6b514514f
Raffaele Francesco Marrazzo <raffaele.marrazzo@studio.unibo.it>
2024-02-20 17:01:28 +0100

    Used MVC estimated wait times controller in the related FXML controller

--- src/main/java/org/apdb4j/view/guests/LiveEstimatedWaitTimesController.java
+++ src/main/java/org/apdb4j/view/guests/LiveEstimatedWaitTimesController.java
@@ -1,6 +1,5 @@
 package org.apdb4j.view.guests;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -12,58 +11,61 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
-import org.apdb4j.db.tables.ParkServices;
-import org.apdb4j.db.tables.RideDetails;
-import org.apdb4j.db.tables.Rides;
-import org.apdb4j.util.QueryBuilder;
+import lombok.NonNull;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apdb4j.controllers.guests.RidesEstimatedWaitTimesController;
+import org.apdb4j.controllers.guests.RidesEstimatedWaitTimesControllerImpl;
 import org.apdb4j.view.FXMLController;
 
 import java.net.URL;
 import java.util.*;
+import java.util.function.Supplier;
 
 /**
  * FXML controller for the screen that allows the user to see the estimated wait times for all the rides.
  */
-// TODO: remove model references. Only for testing purposes.
-@SuppressFBWarnings("NP_NULL_ON_SOME_PATH") // TODO: remove. The code that causes the false positive should be in the controller.
 public class LiveEstimatedWaitTimesController implements FXMLController, Initializable {
 
     private static final double FILTERS_MENU_WIDTH = 250;
     private static final double RIDE_INFO_FONT_SIZE = 18;
     private static final double FILTERS_TITLE_FONT_SIZE = 14;
     private static final double ATTRIBUTE_NAME_FONT_SIZE = 12;
-    private static final double CHECKBOXES_PREF_WIDTH = 100;
-
-
     @FXML
     private BorderPane pane;
-
     @FXML
     private ListView<Label> ridesListView;
-
     @FXML
     private Button showFiltersButton;
-
+    private final ScrollPane filtersScrollableContainer = new ScrollPane();
+    private final ToolBar filtersToolBar = new ToolBar();
     @FXML
     private ComboBox<String> sortMenu;
-    private final Set<String> sortMenuFields = Set.of("Name", "Wait time");
-
-    private final ScrollPane filtersScrollableContainer = new ScrollPane();
-
-    private final ToolBar filtersToolBar = new ToolBar();
-
-    private final List<CheckBox> filters = new ArrayList<>();
-
+    private final ToggleGroup filters = new ToggleGroup();
+    private final ToggleGroup sortButtons = new ToggleGroup();
     private boolean areFiltersOpen;
+    private final RidesEstimatedWaitTimesController controller = new RidesEstimatedWaitTimesControllerImpl();
 
     /**
      * {@inheritDoc}
      */
     @Override
     public void initialize(final URL location, final ResourceBundle resources) {
-        initializeRidesListView();
-        initializeSortMenu();
+        initializeRidesListView(controller.getRidesWithWaitTimes());
         initializeFiltersMenu();
+    }
+
+    private void addAttributeToFilters(final @NonNull String name,
+                                       final @NonNull Collection<? extends Pair<String, Supplier<List<Map<String, String>>>>>
+                                               valuesWithAction) {
+        final var attributeFilterTitle = new Label(name);
+        attributeFilterTitle.setFont(Font.font(Font.getDefault().getFamily(), FontPosture.ITALIC, ATTRIBUTE_NAME_FONT_SIZE));
+        filtersToolBar.getItems().add(attributeFilterTitle);
+        valuesWithAction.forEach(valueWithAction -> {
+            final RadioButton filterOptionButton = new RadioButton(valueWithAction.getKey());
+            filterOptionButton.setToggleGroup(filters);
+            filtersToolBar.getItems().add(filterOptionButton);
+            filterOptionButton.setOnAction(e -> initializeRidesListView(valueWithAction.getValue().get()));
+        });
     }
 
     private void initializeFiltersMenu() {
@@ -78,29 +80,24 @@ public class LiveEstimatedWaitTimesController implements FXMLController, Initial
         filtersTitle.setPadding(new Insets(0, 0, 5, 0));
         filtersToolBar.getItems().add(filtersTitle);
 
-        final var intensityFilterTitle = new Label("Intensity");
-        intensityFilterTitle.setFont(Font.font(Font.getDefault().getFamily(), FontPosture.ITALIC, ATTRIBUTE_NAME_FONT_SIZE));
-        filtersToolBar.getItems().add(intensityFilterTitle);
+        final var sortByTitle = new Label("Sort by");
+        sortByTitle.setFont(Font.font(Font.getDefault().getFamily(), FontPosture.ITALIC, ATTRIBUTE_NAME_FONT_SIZE));
+        filtersToolBar.getItems().add(sortByTitle);
+        initializeSortFields();
 
-        final var intensities = new QueryBuilder().createConnection()
-                .queryAction(db -> db.selectDistinct(Rides.RIDES.INTENSITY)
-                        .from(Rides.RIDES)
-                        .fetch())
-                .closeConnection().getResultAsRecords();
-        intensities.forEach(intensity -> {
-            final var intensityCheckbox = new CheckBox(Objects.requireNonNull(intensity.get(0)).toString());
-            filters.add(intensityCheckbox);
-            filtersToolBar.getItems().add(intensityCheckbox);
-        });
+        controller.getFiltersWithValuesAndAction().forEach(this::addAttributeToFilters);
 
-        filters.forEach(checkBox -> checkBox.setPrefWidth(CHECKBOXES_PREF_WIDTH));
-
-        final var applyFiltersButton = new Button("Apply filters");
-        applyFiltersButton.setCursor(Cursor.HAND);
-        filtersToolBar.getItems().add(applyFiltersButton);
         final var resetFiltersButton = new Button("Reset filters");
         resetFiltersButton.setCursor(Cursor.HAND);
-        resetFiltersButton.setOnAction(event -> filters.forEach(filter -> filter.setSelected(false)));
+        resetFiltersButton.setOnAction(event -> {
+            if (filters.getSelectedToggle() != null) {
+                filters.getSelectedToggle().setSelected(false);
+            }
+            if (sortButtons.getSelectedToggle() != null) {
+                sortButtons.getSelectedToggle().setSelected(false);
+            }
+            initializeRidesListView(controller.getRidesWithWaitTimes());
+        });
         filtersToolBar.getItems().add(resetFiltersButton);
     }
 
@@ -121,34 +118,34 @@ public class LiveEstimatedWaitTimesController implements FXMLController, Initial
         }
     }
 
-    private void initializeRidesListView() {
-        final var rideDetails = new QueryBuilder().createConnection()
-                .queryAction(db -> db.select(ParkServices.PARK_SERVICES.NAME,
-                                Rides.RIDES.INTENSITY,
-                                RideDetails.RIDE_DETAILS.ESTIMATEDWAITTIME, RideDetails.RIDE_DETAILS.STATUS)
-                        .from(ParkServices.PARK_SERVICES, Rides.RIDES, RideDetails.RIDE_DETAILS)
-                        .where(ParkServices.PARK_SERVICES.PARKSERVICEID.eq(Rides.RIDES.RIDEID)
-                                .and(Rides.RIDES.RIDEID.eq(RideDetails.RIDE_DETAILS.RIDEID)))
-                        .fetch())
-                .closeConnection()
-                .getResultAsRecords();
-        for (final var rideDetail : rideDetails) {
-            final var rideDetailLabel = new Label();
-            rideDetailLabel.setFont(new Font(RIDE_INFO_FONT_SIZE));
-            if (Objects.equals(RideDetails.RIDE_DETAILS.STATUS.getValue(rideDetail), "O")) {
-                rideDetailLabel.setText("Ride: " + ParkServices.PARK_SERVICES.NAME.getValue(rideDetail)
-                        + " Intensity: " + Rides.RIDES.INTENSITY.getValue(rideDetail) + " Estimated wait time: "
-                        + RideDetails.RIDE_DETAILS.ESTIMATEDWAITTIME.getValue(rideDetail));
-            } else {
-                rideDetailLabel.setText("Ride: " + ParkServices.PARK_SERVICES.NAME.getValue(rideDetail)
-                        + " Intensity: " + Rides.RIDES.INTENSITY.getValue(rideDetail) + " Estimated wait time: ride closed");
-            }
-            ridesListView.getItems().add(rideDetailLabel);
-        }
+    private void initializeRidesListView(final @NonNull Collection<Map<String, String>> rides) {
+        ridesListView.getItems().clear();
+        rides.forEach(ride -> {
+            final Map<String, String> rideEstimatedWaitTimesInfo = new HashMap<>(ride);
+            final var label = new Label();
+            label.setFont(new Font(RIDE_INFO_FONT_SIZE));
+            label.setText("Name: " + ride.get("Name") + " - ");
+            rideEstimatedWaitTimesInfo.remove("Name");
+            rideEstimatedWaitTimesInfo.forEach((attribute, value) ->
+                    label.setText(label.getText() + attribute + ": " + value + " - "));
+            ridesListView.getItems().add(label);
+        });
+        ridesListView.getItems().forEach(label -> {
+            final var labelText = label.getText();
+            label.setText(new StringBuilder(labelText).replace(labelText.length() - 3, labelText.length(), "").toString());
+        });
     }
 
-    private void initializeSortMenu() {
-        sortMenuFields.forEach(field -> sortMenu.getItems().addAll(field + " (ascending)", field + " (descending)"));
-        sortMenu.getItems().add("No sorting");
+    private void initializeSortFields() {
+        controller.getSortOptionsWithActions().forEach((sortOption, action) -> {
+            final RadioButton sortOptionButton = new RadioButton(sortOption);
+            sortOptionButton.setOnAction(e -> {
+                if (((RadioButton) e.getSource()).isSelected()) {
+                    initializeRidesListView(action.get());
+                }
+            });
+            filtersToolBar.getItems().add(sortOptionButton);
+            sortOptionButton.setToggleGroup(sortButtons);
+        });
     }
 }
