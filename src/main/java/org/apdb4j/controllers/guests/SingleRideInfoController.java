a139c685097a3795c6538fbc80b18a256631392c
Raffaele Francesco Marrazzo <raffaele.marrazzo@studio.unibo.it>
2024-02-20 15:39:34 +0100

    SingleParkServiceInfoController refactoring: now the method that formats the values for the GUI is in an abstract class

--- src/main/java/org/apdb4j/controllers/guests/SingleRideInfoController.java
+++ src/main/java/org/apdb4j/controllers/guests/SingleRideInfoController.java
@@ -1,36 +1,16 @@
 package org.apdb4j.controllers.guests;
 
 import lombok.NonNull;
-import org.apache.commons.lang3.tuple.ImmutablePair;
 import org.apdb4j.util.QueryBuilder;
-import org.jooq.Field;
-import org.jooq.Record;
-import org.jooq.SelectFieldOrAsterisk;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 import static org.apdb4j.db.Tables.*;
-import static org.apdb4j.db.Tables.RIDE_DETAILS;
 
 /**
  * Implementation of a single info controller for rides.
  */
-public class SingleRideInfoController implements SingleParkServiceInfoController {
-
-    private List<Record> actualContent = new ArrayList<>();
-
-    private static final List<Field<?>> ALL_INFO_FIELDS = List.of(RIDES.INTENSITY,
-            RIDES.DURATION,
-            RIDES.MAXSEATS,
-            RIDES.MINHEIGHT,
-            RIDES.MAXHEIGHT,
-            RIDES.MINWEIGHT,
-            RIDES.MAXWEIGHT,
-            FACILITIES.OPENINGTIME,
-            FACILITIES.CLOSINGTIME,
-            PARK_SERVICES.TYPE,
-            RIDE_DETAILS.STATUS);
+public class SingleRideInfoController extends AbstractSingleParkServiceInfoController {
 
     /**
      * {@inheritDoc}
@@ -45,8 +25,18 @@ public class SingleRideInfoController implements SingleParkServiceInfoController
      */
     @Override
     public Map<String, String> getAllParkServiceInfo(final @NonNull String parkServiceName) {
-        actualContent = new QueryBuilder().createConnection()
-                .queryAction(db -> db.select(ALL_INFO_FIELDS.toArray(new SelectFieldOrAsterisk[]{}))
+        return formatQueryResult(new QueryBuilder().createConnection()
+                .queryAction(db -> db.select(RIDES.INTENSITY,
+                                RIDES.DURATION,
+                                RIDES.MAXSEATS,
+                                RIDES.MINHEIGHT,
+                                RIDES.MAXHEIGHT,
+                                RIDES.MINWEIGHT,
+                                RIDES.MAXWEIGHT,
+                                FACILITIES.OPENINGTIME,
+                                FACILITIES.CLOSINGTIME,
+                                PARK_SERVICES.TYPE,
+                                RIDE_DETAILS.STATUS)
                         .from(PARK_SERVICES)
                         .join(FACILITIES).on(PARK_SERVICES.PARKSERVICEID.eq(FACILITIES.FACILITYID))
                         .join(RIDES).on(FACILITIES.FACILITYID.eq(RIDES.RIDEID))
@@ -54,14 +44,6 @@ public class SingleRideInfoController implements SingleParkServiceInfoController
                         .where(PARK_SERVICES.NAME.eq(parkServiceName))
                         .fetch())
                 .closeConnection()
-                .getResultAsRecords();
-        return formatActualContent().get(0);
-    }
-
-    private List<Map<String, String>> formatActualContent() {
-        return actualContent.stream().map(ride -> Arrays.stream(ride.fields())
-                        .map(field -> new ImmutablePair<>(field.getName(), ride.get(field).toString()))
-                        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)))
-                .toList();
+                .getResultAsRecords().get(0));
     }
 }
