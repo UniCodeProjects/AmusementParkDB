042060242eda1044ce93e750c2768c711643ba8a
Raffaele Francesco Marrazzo <raffaele.marrazzo@studio.unibo.it>
2024-02-20 15:39:34 +0100

    SingleParkServiceInfoController refactoring: now the method that formats the values for the GUI is in an abstract class

---
+++ src/main/java/org/apdb4j/controllers/guests/AbstractSingleParkServiceInfoController.java
@@ -0,0 +1,27 @@
+package org.apdb4j.controllers.guests;
+
+import lombok.NonNull;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.jooq.Record;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+/**
+ * Abstract class for {@link SingleParkServiceInfoController}s.
+ */
+abstract class AbstractSingleParkServiceInfoController implements SingleParkServiceInfoController {
+
+    /**
+     * Formats the query result into a {@code Map}, that can be easily handled by the view component of the
+     * MVC architecture.
+     * @param parkServiceInfo the query result that contains all the required information about a park service.
+     * @return all the information about the park service well-formatted for the view component.
+     */
+    protected Map<String, String> formatQueryResult(final @NonNull Record parkServiceInfo) {
+        return Arrays.stream(parkServiceInfo.fields())
+                .map(field -> new ImmutablePair<>(field.getName(), parkServiceInfo.get(field).toString()))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+}
