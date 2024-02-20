314f8a2043e0519f6537e3baf7e78de0e4f97405
Raffaele Francesco Marrazzo <raffaele.marrazzo@studio.unibo.it>
2024-02-20 20:40:17 +0100

    Removed duplicate code from park service controllers. Improved park service controllers architecture

---
+++ src/main/java/org/apdb4j/controllers/guests/AbstractParkServiceController.java
@@ -0,0 +1,77 @@
+package org.apdb4j.controllers.guests;
+
+import lombok.NonNull;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.jooq.Record;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+
+import static org.apdb4j.db.Tables.PARK_SERVICES;
+
+/**
+ * Abstract class that contains functionalities that are common to all the {@link ParkServiceController}s.
+ */
+abstract class AbstractParkServiceController implements ParkServiceController {
+
+    private List<Record> actualContent = new ArrayList<>();
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Map<String, Supplier<List<Map<String, String>>>> getSortOptionsWithActions() {
+        return Map.of("Average rating (" + Order.ASCENDING.getName() + ")", () -> sortByAverageRating(Order.ASCENDING),
+                "Average rating (" + Order.DESCENDING.getName() + ")", () -> sortByAverageRating(Order.DESCENDING),
+                "Name (" + Order.ASCENDING.getName() + ")", () -> sortByName(Order.ASCENDING),
+                "Name (" + Order.DESCENDING.getName() + ")", () -> sortByName(Order.DESCENDING));
+    }
+
+    private List<Map<String, String>> sortByName(final @NonNull Order order) {
+        actualContent = actualContent.stream().sorted((ride1, ride2) -> switch (order) {
+            case ASCENDING -> ride1.get(PARK_SERVICES.NAME).compareTo(ride2.get(PARK_SERVICES.NAME));
+            case DESCENDING -> ride2.get(PARK_SERVICES.NAME).compareTo(ride1.get(PARK_SERVICES.NAME));
+        }).toList();
+        return formatActualContent();
+    }
+
+    private List<Map<String, String>> sortByAverageRating(final @NonNull Order order) {
+        setActualContent(getActualContent().stream().sorted((ride1, ride2) -> switch (order) {
+            case ASCENDING -> ride1.get(PARK_SERVICES.AVGRATING).compareTo(ride2.get(PARK_SERVICES.AVGRATING));
+            case DESCENDING -> ride2.get(PARK_SERVICES.AVGRATING).compareTo(ride1.get(PARK_SERVICES.AVGRATING));
+        }).toList());
+        return formatActualContent();
+    }
+
+    /**
+     * Formats the data that have to be displayed in the view such that the view component can handle them.
+     * @return the data well-formatted for the GUI.
+     */
+    protected List<Map<String, String>> formatActualContent() {
+        return actualContent.stream().map(ride -> Arrays.stream(ride.fields())
+                        .map(field -> new ImmutablePair<>(field.getName(), ride.get(field).toString()))
+                        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)))
+                .toList();
+    }
+
+    /**
+     * Allows the subclasses to set their actual content.
+     * The actual content changes as a result of sorting or filtering actions.
+     * @param newContent the data that have to be displayed in the GUI.
+     */
+    protected void setActualContent(final @NonNull List<Record> newContent) {
+        this.actualContent = List.copyOf(newContent);
+    }
+
+    /**
+     * Retrieves the actually displayed content in a way that it could be handled by the controller.
+     * @return the currently displayed data.
+     */
+    protected @NonNull List<Record> getActualContent() {
+        return List.copyOf(actualContent);
+    }
+}
--- src/main/java/org/apdb4j/controllers/guests/RidesEstimatedWaitTimesControllerImpl.java
+++ src/main/java/org/apdb4j/controllers/guests/RidesEstimatedWaitTimesControllerImpl.java
@@ -1,28 +1,27 @@
 package org.apdb4j.controllers.guests;
 
 import lombok.NonNull;
-import org.apache.commons.lang3.tuple.ImmutablePair;
-import org.apache.commons.lang3.tuple.Pair;
 import org.apdb4j.util.QueryBuilder;
-import org.jooq.Field;
-import org.jooq.Record;
-import org.jooq.SelectFieldOrAsterisk;
 
 import java.util.*;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.apdb4j.db.Tables.*;
 
 /**
  * Implementation of a {@link RidesEstimatedWaitTimesController}.
  */
-public class RidesEstimatedWaitTimesControllerImpl implements RidesEstimatedWaitTimesController {
+public class RidesEstimatedWaitTimesControllerImpl extends AbstractRideController
+        implements RidesEstimatedWaitTimesController {
 
-    private static final List<Field<?>> ESTIMATED_WAIT_TIMES_OVERVIEW_FIELDS = List.of(PARK_SERVICES.NAME,
-            RIDES.INTENSITY,
-            RIDE_DETAILS.ESTIMATEDWAITTIME);
-    private List<Record> actualContent = new ArrayList<>();
+    /**
+     * Default constructor.
+     */
+    public RidesEstimatedWaitTimesControllerImpl() {
+        super(PARK_SERVICES.NAME, PARK_SERVICES.AVGRATING, RIDES.INTENSITY, RIDE_DETAILS.ESTIMATEDWAITTIME);
+    }
 
     /**
      * {@inheritDoc}
@@ -37,27 +36,13 @@ public class RidesEstimatedWaitTimesControllerImpl implements RidesEstimatedWait
      */
     @Override
     public Map<String, Supplier<List<Map<String, String>>>> getSortOptionsWithActions() {
-        return Map.of("Estimated wait time (" + Order.ASCENDING.getName() + ")", () -> sortByEstimatedWaitTimes(Order.ASCENDING),
-                "Estimated wait time (" + Order.DESCENDING.getName() + ")", () -> sortByEstimatedWaitTimes(Order.DESCENDING),
-                "Name (" + Order.ASCENDING.getName() + ")", () -> sortByName(Order.ASCENDING),
-                "Name (" + Order.DESCENDING.getName() + ")", () -> sortByName(Order.DESCENDING));
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public Map<String, Collection<? extends Pair<String, Supplier<List<Map<String, String>>>>>> getFiltersWithValuesAndAction() {
-        return Map.of("Intensity", new QueryBuilder().createConnection()
-                .queryAction(db -> db.selectDistinct(RIDES.INTENSITY)
-                        .from(RIDES)
-                        .fetch())
-                .closeConnection()
-                .getResultAsRecords()
-                .getValues(RIDES.INTENSITY).stream()
-                .map(intensity -> new ImmutablePair<String, Supplier<List<Map<String, String>>>>(intensity, () ->
-                        filterByIntensity(intensity)))
-                .toList());
+        final Stream<Map.Entry<String, Supplier<List<Map<String, String>>>>> newEntriesStream = Stream.of(
+                Map.entry("Estimated wait time (" + Order.ASCENDING.getName() + ")",
+                        () -> sortByEstimatedWaitTimes(Order.ASCENDING)),
+                Map.entry("Estimated wait time (" + Order.DESCENDING.getName() + ")",
+                        () -> sortByEstimatedWaitTimes(Order.DESCENDING)));
+        return Stream.concat(newEntriesStream, super.getSortOptionsWithActions().entrySet().stream())
+                .collect(Collectors.toUnmodifiableMap(Map.Entry::getKey, Map.Entry::getValue));
     }
 
     /**
@@ -65,48 +50,21 @@ public class RidesEstimatedWaitTimesControllerImpl implements RidesEstimatedWait
      */
     @Override
     public Collection<Map<String, String>> getRidesWithWaitTimes() {
-        actualContent = new QueryBuilder().createConnection()
-                .queryAction(db -> db.select(ESTIMATED_WAIT_TIMES_OVERVIEW_FIELDS.toArray(new SelectFieldOrAsterisk[]{}))
+        setActualContent(new QueryBuilder().createConnection()
+                .queryAction(db -> db.select(getSelectFields())
                         .from(PARK_SERVICES).join(RIDES).on(PARK_SERVICES.PARKSERVICEID.eq(RIDES.RIDEID))
                         .join(RIDE_DETAILS).on(RIDES.RIDEID.eq(RIDE_DETAILS.RIDEID))
                         .fetch())
                 .closeConnection()
-                .getResultAsRecords();
-        return formatActualContent();
-    }
-
-    private List<Map<String, String>> filterByIntensity(final String intensity) {
-        actualContent = new QueryBuilder().createConnection()
-                .queryAction(db -> db.select(ESTIMATED_WAIT_TIMES_OVERVIEW_FIELDS.toArray(new SelectFieldOrAsterisk[]{}))
-                        .from(PARK_SERVICES).join(RIDES).on(PARK_SERVICES.PARKSERVICEID.eq(RIDES.RIDEID))
-                        .join(RIDE_DETAILS).on(RIDES.RIDEID.eq(RIDE_DETAILS.RIDEID))
-                        .where(RIDES.INTENSITY.eq(intensity))
-                        .fetch())
-                .closeConnection()
-                .getResultAsRecords();
-        return formatActualContent();
-    }
-
-    private List<Map<String, String>> sortByName(final @NonNull Order order) {
-        actualContent = actualContent.stream().sorted((ride1, ride2) -> switch (order) {
-            case ASCENDING -> ride1.get(PARK_SERVICES.NAME).compareTo(ride2.get(PARK_SERVICES.NAME));
-            case DESCENDING -> ride2.get(PARK_SERVICES.NAME).compareTo(ride1.get(PARK_SERVICES.NAME));
-        }).toList();
+                .getResultAsRecords());
         return formatActualContent();
     }
 
     private List<Map<String, String>> sortByEstimatedWaitTimes(final @NonNull Order order) {
-        actualContent = actualContent.stream().sorted((ride1, ride2) -> switch (order) {
+        setActualContent(getActualContent().stream().sorted((ride1, ride2) -> switch (order) {
             case ASCENDING -> ride1.get(RIDE_DETAILS.ESTIMATEDWAITTIME).compareTo(ride2.get(RIDE_DETAILS.ESTIMATEDWAITTIME));
             case DESCENDING -> ride2.get(RIDE_DETAILS.ESTIMATEDWAITTIME).compareTo(ride1.get(RIDE_DETAILS.ESTIMATEDWAITTIME));
-        }).toList();
+        }).toList());
         return formatActualContent();
-    }
-
-    private List<Map<String, String>> formatActualContent() {
-        return actualContent.stream().map(ride -> Arrays.stream(ride.fields())
-                        .map(field -> new ImmutablePair<>(field.getName(), ride.get(field).toString()))
-                        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)))
-                .toList();
     }
 }
