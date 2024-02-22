7242e7acba533446866624968530fcea61d13ff9
Raffaele Francesco Marrazzo <raffaele.marrazzo@studio.unibo.it>
2024-02-22 11:37:16 +0100

    Moved implementation of ParkServiceController.getParkServiceID() in ParkServiceManager

--- src/main/java/org/apdb4j/controllers/guests/ParkServiceController.java
+++ src/main/java/org/apdb4j/controllers/guests/ParkServiceController.java
@@ -1,17 +1,12 @@
 package org.apdb4j.controllers.guests;
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import lombok.NonNull;
 import org.apache.commons.lang3.tuple.Pair;
 import org.apdb4j.controllers.Controller;
-import org.apdb4j.util.QueryBuilder;
-import org.jooq.Record;
-import org.jooq.Result;
+import org.apdb4j.core.managers.ParkServiceManager;
 
 import java.util.*;
 import java.util.function.Supplier;
-
-import static org.apdb4j.db.Tables.PARK_SERVICES;
 
 /**
  * MVC controller of any park service.
@@ -33,23 +28,11 @@ public interface ParkServiceController extends Controller {
 
     /**
      * Returns the identifier of the park service with the provided name, if exists,
-     * otherwise an {@link IllegalArgumentException will be thrown}.
+     * otherwise an {@link IllegalArgumentException} will be thrown.
      * @param parkServiceName the name of the park service.
      * @return the identifier of the park service with the provided name.
      */
-    @SuppressFBWarnings("NP_NULL_ON_SOME_PATH")
     static String getParkServiceID(final @NonNull String parkServiceName) {
-        final Result<Record> parkServiceID = Objects.requireNonNull(new QueryBuilder().createConnection()
-                .queryAction(db -> db.select(PARK_SERVICES.PARKSERVICEID)
-                        .from(PARK_SERVICES)
-                        .where(PARK_SERVICES.NAME.eq(parkServiceName))
-                        .fetch())
-                .closeConnection()
-                .getResultAsRecords());
-        if (parkServiceID.isEmpty()) {
-            throw new IllegalArgumentException("There is no park service with the provided name");
-        } else {
-            return parkServiceID.getValue(0, PARK_SERVICES.PARKSERVICEID);
-        }
+        return ParkServiceManager.getParkServiceID(parkServiceName);
     }
 }
