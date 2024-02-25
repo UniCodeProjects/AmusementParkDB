ee9280109e469c1d3b7be1e63aeb365840d0ea6f
Raffaele Francesco Marrazzo <raffaele.marrazzo@studio.unibo.it>
2024-02-25 14:05:58 +0100

    Added org.jooq.SortOrder field in enum Order

--- src/main/java/org/apdb4j/controllers/guests/Order.java
+++ src/main/java/org/apdb4j/controllers/guests/Order.java
@@ -2,6 +2,8 @@ package org.apdb4j.controllers.guests;
 
 import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NonNull;
+import org.jooq.SortOrder;
 
 /**
  * Enum that represents the sorting order.
@@ -11,16 +13,19 @@ public enum Order {
     /**
      * The ascending sorting order.
      */
-    ASCENDING("ascending"),
+    ASCENDING("ascending", SortOrder.ASC),
     /**
      * The descending sorting order.
      */
-    DESCENDING("descending");
+    DESCENDING("descending", SortOrder.DESC);
 
     @Getter(AccessLevel.PACKAGE)
     private final String name;
+    @Getter(AccessLevel.PACKAGE)
+    private final SortOrder order;
 
-    Order(final String name) {
+    Order(final @NonNull String name, final @NonNull SortOrder order) {
         this.name = name;
+        this.order = order;
     }
 }
