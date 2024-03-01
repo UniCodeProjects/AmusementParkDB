0d482b7b5deca672672cdc12ec96856da77e6114
Raffaele Francesco Marrazzo <raffaele.marrazzo@studio.unibo.it>
2024-03-01 11:56:01 +0100

    Implemented methods to retrieve the personal data for an account. Used them in the GUI

--- src/main/java/org/apdb4j/controllers/SessionManager.java
+++ src/main/java/org/apdb4j/controllers/SessionManager.java
@@ -2,6 +2,7 @@ package org.apdb4j.controllers;
 
 import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NonNull;
 import org.apdb4j.core.managers.AccountManager;
 
 import java.util.Objects;
@@ -32,20 +33,78 @@ public final class SessionManager {
      */
     public void login(final String accountUsername) {
         if (Objects.isNull(session)) {
-            session = new SessionInfo(accountUsername,
+            session = new SessionInfo(AccountManager.getAccountOwnerName(accountUsername),
+                    AccountManager.getAccountOwnerSurname(accountUsername),
+                    accountUsername,
                     AccountManager.getAccountEmail(accountUsername),
                     AccountManager.getPersonID(accountUsername));
         }
     }
 
     /**
+     * Changes the name of the owner of the currently logged account.
+     * @param newName the new name of the owner of the currently logged account.
+     * @return {@code true} if the update is successfully, {@code false} otherwise.
+     */
+    public boolean changeLoggedAccountOwnerName(final @NonNull String newName) {
+        if (!AccountManager.changeAccountOwnerName(session.email(), newName)) {
+            return false;
+        }
+        session = new SessionInfo(newName, session.surname(), session.username(), session.email(), session.personID());
+        return true;
+    }
+
+    /**
+     * Changes the surname of the owner of the currently logged account.
+     * @param newSurname the new surname of the owner of the currently logged account.
+     * @return {@code true} if the update is successfully, {@code false} otherwise.
+     */
+    public boolean changeLoggedAccountOwnerSurname(final @NonNull String newSurname) {
+        if (!AccountManager.changeAccountOwnerSurname(session.email(), newSurname)) {
+            return false;
+        }
+        session = new SessionInfo(session.name(), newSurname, session.username(), session.email(), session.personID());
+        return true;
+    }
+
+    /**
+     * Changes the username of the currently logged account with the provided one.
+     * @param newUsername the new username of the currently logged account.
+     * @return {@code true} if the update is successfully, {@code false} otherwise (e.g. if another account has
+     *         {@code newUsername} as its username).
+     */
+    public boolean changeLoggedAccountUsername(final @NonNull String newUsername) {
+        if (!AccountManager.changeUsername(session.email(), newUsername)) {
+            return false;
+        }
+        session = new SessionInfo(session.name(), session.surname(), newUsername, session.email(), session.personID());
+        return true;
+    }
+
+    /**
+     * Changes the email of the currently logged account with the provided one.
+     * @param newEmail the new email of the currently logged account.
+     * @return {@code true} if the update is successfully, {@code false} otherwise (e.g. if another account has
+     *         {@code newEmail} as its email).
+     */
+    public boolean changeLoggedAccountEmail(final @NonNull String newEmail) {
+        if (!AccountManager.changeEmail(session.email(), newEmail)) {
+            return false;
+        }
+        session = new SessionInfo(session.name(), session.surname(), session.username(), newEmail, session.personID());
+        return true;
+    }
+
+    /**
      * Record that keeps track of the username of the logged account and of the person ID
      * of the owner of said account.
+     * @param name the name of the owner of the account that is logged in.
+     * @param surname the surname of the owner of the account that is logged in.
      * @param username the username of the account that is logged in.
      * @param email the email of the account that is logged in.
      * @param personID the identifier of the owner of the logged account.
      */
-    public record SessionInfo(String username, String email, String personID) {
+    public record SessionInfo(String name, String surname, String username, String email, String personID) {
         /**
          * Checks if the logged user is an admin.
          * @return {@code true} if an admin
