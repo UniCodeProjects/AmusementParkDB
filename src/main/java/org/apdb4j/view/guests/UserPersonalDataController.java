ed72d09672d5c5894fd015b5f682b188a4aa6bff
Raffaele Francesco Marrazzo <raffaele.marrazzo@studio.unibo.it>
2024-03-01 11:56:01 +0100

    Implemented methods to retrieve the personal data for an account. Used them in the GUI

--- src/main/java/org/apdb4j/view/guests/UserPersonalDataController.java
+++ src/main/java/org/apdb4j/view/guests/UserPersonalDataController.java
@@ -2,8 +2,11 @@ package org.apdb4j.view.guests;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import org.apdb4j.controllers.SessionManager;
+import org.apdb4j.util.view.AlertBuilder;
 import org.apdb4j.util.view.JavaFXUtils;
 import org.apdb4j.util.view.LoadFXML;
 import org.apdb4j.view.BackableAbstractFXMLController;
@@ -71,19 +74,48 @@ public class UserPersonalDataController extends BackableAbstractFXMLController {
         confirmNewNameButton.setOnAction(event -> {
             confirmNewNameButton.setVisible(false);
             nameTextField.setEditable(false);
+            if (!SessionManager.getSessionManager().changeLoggedAccountOwnerName(nameTextField.getText())) {
+                new AlertBuilder(Alert.AlertType.ERROR).setContentText("The provided name is not valid").show();
+                nameTextField.setText(SessionManager.getSessionManager().getSession().name());
+            } else {
+                new AlertBuilder(Alert.AlertType.INFORMATION).setContentText("Name modified successfully!").show();
+            }
         });
         confirmNewSurnameButton.setOnAction(event -> {
             confirmNewSurnameButton.setVisible(false);
             surnameTextField.setEditable(false);
+            if (!SessionManager.getSessionManager().changeLoggedAccountOwnerSurname(surnameTextField.getText())) {
+                new AlertBuilder(Alert.AlertType.ERROR).setContentText("The provided surname is not valid").show();
+                surnameTextField.setText(SessionManager.getSessionManager().getSession().surname());
+            } else {
+                new AlertBuilder(Alert.AlertType.INFORMATION).setContentText("Surname modified successfully!").show();
+            }
         });
         confirmNewUsernameButton.setOnAction(event -> {
             confirmNewUsernameButton.setVisible(false);
             usernameTextField.setEditable(false);
+            if (!SessionManager.getSessionManager().changeLoggedAccountUsername(usernameTextField.getText())) {
+                new AlertBuilder(Alert.AlertType.ERROR).setContentText("The provided username is not valid").show();
+                usernameTextField.setText(SessionManager.getSessionManager().getSession().username());
+            } else {
+                new AlertBuilder(Alert.AlertType.INFORMATION).setContentText("Username modified successfully!").show();
+            }
         });
         confirmNewEmailButton.setOnAction(event -> {
             confirmNewEmailButton.setVisible(false);
             emailTextField.setEditable(false);
+            if (!SessionManager.getSessionManager().changeLoggedAccountEmail(emailTextField.getText())) {
+                new AlertBuilder(Alert.AlertType.ERROR).setContentText("The provided email is not valid").show();
+                emailTextField.setText(SessionManager.getSessionManager().getSession().email());
+            } else {
+                new AlertBuilder(Alert.AlertType.INFORMATION).setContentText("Email updated successfully!").show();
+            }
         });
+
+        nameTextField.setText(SessionManager.getSessionManager().getSession().name());
+        surnameTextField.setText(SessionManager.getSessionManager().getSession().surname());
+        usernameTextField.setText(SessionManager.getSessionManager().getSession().username());
+        emailTextField.setText(SessionManager.getSessionManager().getSession().email());
     }
 
     /**
