2e63f4d8226a2dc5af71b9f578a570aa32df5a66
Raffaele Francesco Marrazzo <raffaele.marrazzo@studio.unibo.it>
2024-03-02 15:56:20 +0100

    wip: implementing MVC controller for the tickets bought by the currently logged user

--- src/main/java/org/apdb4j/view/guests/UserTicketsScreenController.java
+++ src/main/java/org/apdb4j/view/guests/UserTicketsScreenController.java
@@ -3,7 +3,10 @@ package org.apdb4j.view.guests;
 import javafx.fxml.FXML;
 import javafx.scene.control.Hyperlink;
 import javafx.scene.control.ListView;
-import org.apdb4j.util.view.LoadFXML;
+import lombok.NonNull;
+import org.apdb4j.controllers.guests.BoughtTicketsController;
+import org.apdb4j.controllers.guests.BoughtTicketsControllerImpl;
+import org.apdb4j.controllers.guests.TicketType;
 import org.apdb4j.view.BackableAbstractFXMLController;
 
 import java.net.URL;
@@ -16,6 +19,17 @@ public class UserTicketsScreenController extends BackableAbstractFXMLController 
 
     @FXML
     private ListView<Hyperlink> listView;
+    private final BoughtTicketsController controller;
+    private final TicketType ticketType;
+
+    /**
+     * Creates a new instance of this class that will refer to the tickets of the provided type.
+     * @param ticketType the ticket type that the new instance will be able to handle.
+     */
+    public UserTicketsScreenController(final @NonNull TicketType ticketType) {
+        this.ticketType = ticketType;
+        controller = new BoughtTicketsControllerImpl(ticketType);
+    }
 
     /**
      * {@inheritDoc}
@@ -23,14 +37,13 @@ public class UserTicketsScreenController extends BackableAbstractFXMLController 
     @Override
     public void initialize(final URL location, final ResourceBundle resources) {
         super.initialize(location, resources);
-        addTicket();
-    }
-
-    private void addTicket() {
-        final var ticket = new Hyperlink("3 tickets, valid on 25/04");
-        ticket.setFocusTraversable(false);
-        ticket.setOnAction(event ->
-                LoadFXML.fromEventAsPopup(event, "layouts/tickets-info.fxml", "tickets valid on 25/04", 0.5, 0.5));
-        listView.getItems().add(ticket);
+        controller.getNumberOfBoughtTickets().forEach(dateWithNumber -> {
+            final var hyperlink = new Hyperlink(dateWithNumber.getRight() + " "
+                    + ticketType.getName() + "s, "
+                    + (ticketType.equals(TicketType.SINGLE_DAY_TICKET) ? "valid on " : "valid until ")
+                    + dateWithNumber.getLeft());
+            hyperlink.setFocusTraversable(false);
+            listView.getItems().add(hyperlink);
+        });
     }
 }
