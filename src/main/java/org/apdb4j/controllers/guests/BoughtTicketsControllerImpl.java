5775756c3b9c3c1cd6ab122dbb5117a89e51385d
Raffaele Francesco Marrazzo <raffaele.marrazzo@studio.unibo.it>
2024-03-04 19:30:17 +0100

    Implemented filters in the screen that shows the guest their tickets

--- src/main/java/org/apdb4j/controllers/guests/BoughtTicketsControllerImpl.java
+++ src/main/java/org/apdb4j/controllers/guests/BoughtTicketsControllerImpl.java
@@ -2,14 +2,20 @@ package org.apdb4j.controllers.guests;
 
 import lombok.NonNull;
 import org.apdb4j.controllers.SessionManager;
+import org.apdb4j.core.managers.Manager;
 import org.apdb4j.util.QueryBuilder;
 import org.apdb4j.view.guests.TicketTableItem;
+import org.jooq.Record;
+import org.jooq.Result;
+import org.jooq.types.UInteger;
 
+import java.time.LocalDate;
 import java.time.Year;
 import java.util.Collection;
 import java.util.stream.Collectors;
 
 import static org.apdb4j.db.Tables.ATTRIBUTIONS;
+import static org.apdb4j.db.Tables.PRICE_LISTS;
 import static org.apdb4j.db.Tables.TICKETS;
 
 /**
@@ -32,7 +38,7 @@ public class BoughtTicketsControllerImpl implements BoughtTicketsController {
      */
     @Override
     public Collection<TicketTableItem> getAllData() {
-        return new QueryBuilder().createConnection()
+        return getDataAsTicketTableItem(new QueryBuilder().createConnection()
                 .queryAction(db -> db.select(TICKETS.TICKETID,
                         TICKETS.PURCHASEDATE,
                         TICKETS.VALIDON,
@@ -45,9 +51,96 @@ public class BoughtTicketsControllerImpl implements BoughtTicketsController {
                         .and(ATTRIBUTIONS.TYPE.equalIgnoreCase(ticketType.getName()))
                         .fetch())
                 .closeConnection()
-                .getResultAsRecords()
-                .stream()
-                .map(ticket -> new TicketTableItem(ticket.get(TICKETS.TICKETID),
+                .getResultAsRecords());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Collection<TicketTableItem> filterByCategory(final @NonNull String category) {
+        return getDataAsTicketTableItem(new QueryBuilder().createConnection()
+                .queryAction(db -> db.select(TICKETS.TICKETID,
+                        TICKETS.PURCHASEDATE,
+                        TICKETS.VALIDON,
+                        TICKETS.VALIDUNTIL,
+                        TICKETS.REMAININGENTRANCES,
+                        ATTRIBUTIONS.YEAR,
+                        ATTRIBUTIONS.CATEGORY)
+                        .from(TICKETS).join(ATTRIBUTIONS).on(TICKETS.TICKETID.eq(ATTRIBUTIONS.TICKETID))
+                        .where(TICKETS.OWNERID.eq(SessionManager.getSessionManager().getSession().personID()))
+                        .and(ATTRIBUTIONS.TYPE.equalIgnoreCase(ticketType.getName()))
+                        .and(ATTRIBUTIONS.CATEGORY.equalIgnoreCase(category))
+                        .fetch())
+                .closeConnection().getResultAsRecords());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Collection<TicketTableItem> getValidTickets() {
+        return getDataAsTicketTableItem(new QueryBuilder().createConnection()
+                .queryAction(db -> db.select(TICKETS.TICKETID,
+                        TICKETS.PURCHASEDATE,
+                        TICKETS.VALIDON,
+                        TICKETS.VALIDUNTIL,
+                        TICKETS.REMAININGENTRANCES,
+                        ATTRIBUTIONS.YEAR,
+                        ATTRIBUTIONS.CATEGORY)
+                        .from(TICKETS).join(ATTRIBUTIONS).on(TICKETS.TICKETID.eq(ATTRIBUTIONS.TICKETID))
+                        .where(TICKETS.OWNERID.eq(SessionManager.getSessionManager().getSession().personID()))
+                        .and(ATTRIBUTIONS.TYPE.equalIgnoreCase(ticketType.getName()))
+                        .and(TICKETS.REMAININGENTRANCES.greaterThan(UInteger.valueOf(0)))
+                        .and(ticketType.equals(TicketType.SINGLE_DAY_TICKET)
+                                ? TICKETS.VALIDON.greaterOrEqual(LocalDate.now())
+                                : TICKETS.VALIDUNTIL.greaterOrEqual(LocalDate.now()))
+                        .fetch())
+                .closeConnection().getResultAsRecords());
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Collection<TicketTableItem> getExpiredTickets() {
+        // TODO: perform a single query instead?
+        final var result = getAllData();
+        result.removeAll(getValidTickets());
+        return result;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Collection<Integer> getPriceListYears() {
+        return Manager.viewAllInfoFromTable(PRICE_LISTS).stream().map(record -> record.get(PRICE_LISTS.YEAR)).toList();
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public Collection<TicketTableItem> filterByPriceListYear(final int year) {
+        return getDataAsTicketTableItem(new QueryBuilder().createConnection()
+                .queryAction(db -> db.select(TICKETS.TICKETID,
+                        TICKETS.PURCHASEDATE,
+                        TICKETS.VALIDON,
+                        TICKETS.VALIDUNTIL,
+                        TICKETS.REMAININGENTRANCES,
+                        ATTRIBUTIONS.YEAR,
+                        ATTRIBUTIONS.CATEGORY)
+                        .from(TICKETS).join(ATTRIBUTIONS).on(TICKETS.TICKETID.eq(ATTRIBUTIONS.TICKETID))
+                        .where(TICKETS.OWNERID.eq(SessionManager.getSessionManager().getSession().personID()))
+                        .and(ATTRIBUTIONS.TYPE.equalIgnoreCase(ticketType.getName()))
+                        .and(ATTRIBUTIONS.YEAR.eq(year))
+                        .fetch())
+                .closeConnection().getResultAsRecords());
+    }
+
+    private Collection<TicketTableItem> getDataAsTicketTableItem(final @NonNull Result<Record> data) {
+        return data.stream().map(ticket -> new TicketTableItem(ticket.get(TICKETS.TICKETID),
                         ticket.get(TICKETS.PURCHASEDATE),
                         ticket.get(TICKETS.VALIDON),
                         ticket.get(TICKETS.VALIDUNTIL),
--- src/main/java/org/apdb4j/view/guests/TicketsInfoController.java
+++ src/main/java/org/apdb4j/view/guests/TicketsInfoController.java
@@ -1,16 +1,28 @@
 package org.apdb4j.view.guests;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.geometry.Orientation;
+import javafx.scene.Cursor;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.ToolBar;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
 import lombok.NonNull;
+import org.apache.commons.lang3.StringUtils;
 import org.apdb4j.controllers.SessionManager;
 import org.apdb4j.controllers.guests.BoughtTicketsController;
 import org.apdb4j.controllers.guests.BoughtTicketsControllerImpl;
+import org.apdb4j.controllers.guests.TicketControllerImpl;
 import org.apdb4j.controllers.guests.TicketType;
 import org.apdb4j.view.BackableAbstractFXMLController;
 
@@ -18,6 +30,7 @@ import java.net.URL;
 import java.time.LocalDate;
 import java.time.Year;
 import java.util.List;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 /**
@@ -26,12 +39,21 @@ import java.util.ResourceBundle;
 public class TicketsInfoController extends BackableAbstractFXMLController {
 
     private static final int TITLE_FONT_SIZE = 24;
+    private static final int FILTERS_MENU_WIDTH = 250;
+    @FXML
+    private BorderPane pane;
     @FXML
     private Label title;
+    @FXML
+    private Button showFiltersButton;
     @FXML
     private TableView<TicketTableItem> tableView;
     private final BoughtTicketsController controller;
     private final TicketType ticketType;
+    private boolean areFiltersOpen;
+    private final ScrollPane filterScrollableContainer = new ScrollPane();
+    private final ToolBar filtersToolBar = new ToolBar();
+    private final ToggleGroup filters = new ToggleGroup();
 
     /**
      * Default constructor. Creates a new instance of this class that shows all the info about the tickets
@@ -41,6 +63,23 @@ public class TicketsInfoController extends BackableAbstractFXMLController {
     public TicketsInfoController(final @NonNull TicketType ticketType) {
         this.ticketType = ticketType;
         controller = new BoughtTicketsControllerImpl(ticketType);
+    }
+
+    /**
+     * Opens the filter toolbar.
+     * @param event the click on the "show filters" button.
+     */
+    @FXML
+    void onFilterButtonPressed(final ActionEvent event) {
+        if (!areFiltersOpen) {
+            showFiltersButton.setText("Hide filters");
+            pane.setRight(filterScrollableContainer);
+            areFiltersOpen = true;
+        } else {
+            showFiltersButton.setText("Show filters");
+            pane.getChildren().remove(filterScrollableContainer);
+            areFiltersOpen = false;
+        }
     }
 
     /**
@@ -74,6 +113,77 @@ public class TicketsInfoController extends BackableAbstractFXMLController {
                 yearColumn,
                 categoryColumn));
         tableView.getItems().addAll(controller.getAllData());
+
+        initializeFiltersMenu();
+    }
+
+    private void initializeFiltersMenu() {
+        filterScrollableContainer.setPrefWidth(FILTERS_MENU_WIDTH);
+        filterScrollableContainer.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        filtersToolBar.setPrefWidth(FILTERS_MENU_WIDTH);
+        filtersToolBar.setOrientation(Orientation.VERTICAL);
+        filterScrollableContainer.setContent(filtersToolBar);
+
+        final var filtersTitle = new Label("Filters");
+        filtersTitle.setFont(Font.font(Font.getDefault().getFamily(), FontWeight.BOLD, 14));
+        filtersToolBar.getItems().add(filtersTitle);
+
+        final var categoryFilterTitle = new Label("Category");
+        categoryFilterTitle.setFont(Font.font(Font.getDefault().getFamily(), FontPosture.ITALIC, 12));
+        filtersToolBar.getItems().add(categoryFilterTitle);
+
+        new TicketControllerImpl().getCustomerCategories().forEach(category -> {
+            final RadioButton categoryFilterButton = new RadioButton(StringUtils.capitalize(category));
+            categoryFilterButton.setToggleGroup(filters);
+            filtersToolBar.getItems().add(categoryFilterButton);
+            categoryFilterButton.setOnAction(e -> {
+                tableView.getItems().clear();
+                tableView.getItems().addAll(controller.filterByCategory(category));
+            });
+        });
+
+        final var validityFilterTitle = new Label("Validity");
+        validityFilterTitle.setFont(Font.font(Font.getDefault().getFamily(), FontPosture.ITALIC, 12));
+        filtersToolBar.getItems().add(validityFilterTitle);
+
+        final RadioButton validTicketsButton = new RadioButton("Valid");
+        validTicketsButton.setToggleGroup(filters);
+        filtersToolBar.getItems().add(validTicketsButton);
+        validTicketsButton.setOnAction(e -> {
+            tableView.getItems().clear();
+            tableView.getItems().addAll(controller.getValidTickets());
+        });
+
+        final RadioButton expiredTicketsButton = new RadioButton("Expired");
+        expiredTicketsButton.setToggleGroup(filters);
+        filtersToolBar.getItems().add(expiredTicketsButton);
+        expiredTicketsButton.setOnAction(e -> {
+            tableView.getItems().clear();
+            tableView.getItems().addAll(controller.getExpiredTickets());
+        });
+
+        final var yearFilterTitle = new Label("Year");
+        yearFilterTitle.setFont(Font.font(Font.getDefault().getFamily(), FontPosture.ITALIC, 12));
+        filtersToolBar.getItems().add(yearFilterTitle);
+
+        controller.getPriceListYears().forEach(year -> {
+            final RadioButton yearFilterButton = new RadioButton(year.toString());
+            yearFilterButton.setToggleGroup(filters);
+            filtersToolBar.getItems().add(yearFilterButton);
+            yearFilterButton.setOnAction(e -> {
+                tableView.getItems().clear();
+                tableView.getItems().addAll(controller.filterByPriceListYear(year));
+            });
+        });
+
+        final Button resetFiltersButton = new Button("Reset filters");
+        resetFiltersButton.setOnAction(e -> {
+            Optional.ofNullable(filters.getSelectedToggle()).ifPresent(filterButton -> filterButton.setSelected(false));
+            tableView.getItems().clear();
+            tableView.getItems().addAll(controller.getAllData());
+        });
+        resetFiltersButton.setCursor(Cursor.HAND);
+        filtersToolBar.getItems().add(resetFiltersButton);
     }
 }
 
